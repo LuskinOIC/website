e619724594e183ef90164d67b6f969b524d78ece
Joe Torreggiani <joetorreggiani@gmail.com>
2023-11-01 18:05:59 -0400

    Make linter happy.

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,15 +1,19 @@
-export function getData() {
+async function getData(slug: string) {
   // TODO: Fetch data from Contentful.
   return Promise.resolve({
-    title: "Our Goal is to Make Sure Kids Keep Chasing Theirs",
+    title: slug,
     content:
       "We provide world-class care to patients and their families, putting children in the best hands for a successful diagnosis, treatment and rehabilitation,so they can continue to chase their goals.",
   });
 }
 
-export default async function NewsArticle() {
-  const { title, content } = await getData();
-
+// this function uses the correct typing for nextjs page params
+export default async function NewsArticlePage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { title, content } = await getData(params.slug);
   return (
     <main>
       <h1>{title}</h1>
