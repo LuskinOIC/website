aba608b5b06f8f67456ede5ea07f759cd3b46785
Joe Torreggiani <joetorreggiani@gmail.com>
2023-11-01 19:32:26 -0400

    Simplify code for now.

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,23 +1,7 @@
-async function getData(slug: string) {
-  // TODO: Fetch data from Contentful.
-  return Promise.resolve({
-    title: slug,
-    content:
-      "We provide world-class care to patients and their families, putting children in the best hands for a successful diagnosis, treatment and rehabilitation,so they can continue to chase their goals.",
-  });
-}
-
-// this function uses the correct typing for nextjs page params
-export default async function NewsArticlePage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { title, content } = await getData(params.slug);
+export default function NewsArticlePage() {
   return (
     <main>
-      <h1>{title}</h1>
-      <p>{content}</p>
+      <h1>New Article</h1>
     </main>
   );
 }
