c186f4f1cbfbad38dd7dee9690de6aa6993bf4c2
Khiry <khiryshank@gmail.com>
2023-11-05 20:17:54 -0800

    Changed space layout, font and color

--- app/components/SocialMediaBanner.tsx
+++ app/components/SocialMediaBanner.tsx
@@ -1,13 +1,12 @@
 // import Link from "next/link";
 
 export default function SocialMediaBanner() {
-    // TODO: Replace this with the actual navbar.
-    // Use next/link to link to the pages.
+
     return (
-        <div className="grid grid-rows-3 grid-flow-col gap-4 bg-yellow-500 ">
-          <div className="row-span-3 ...">01</div>
-          <div className="col-span-2 .."><div><h1>Follow our story on social media!</h1></div>Join us in our mission to transform musculoskeletal care for all, one child at a time, through education, research, and unwavering support.<div><p></p></div></div>
-          <div className="row-span-2 col-span-2 ..">Social Media Links</div>
+        <div className="grid grid-rows-3 grid-flow-col gap-4 bg-[#FCE385] "> 
+          <div className="row-span-3 ..."><div className="flex flex-row min-h-screen justify-center items-center">IMAGE PLACE HOLDER</div></div>
+          <div className="col-span-2 .."><div className="text-2xl flex flex-row min-h-screen justify-center items-center text-center"><h1>Follow our story on social media!</h1></div><div className="text-md text-center"><p>Join us in our mission to transform musculoskeletal care for all, one child at a time,</p> <p>through education, research, and unwavering support.</p></div></div>
+          <div className="row-span-2 col-span-2 .. "><div className="text-center">Social Media Links</div></div>
       </div>
     );
   }
