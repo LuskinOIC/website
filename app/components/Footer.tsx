d3dec014e23e4073893582c72041d47e0d4bf1fc
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-06 19:02:32 -0800

    Resolved majority of lint test errors, focusing on remaining few lines-attempt 4

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,9 +9,11 @@ export default function Footer() {
     // footer container
     <div
       id="container"
+
       className="flex justify-around bg-[#0076AD] text-white pt-8 pb-11">
       <div
         id="footer-left-content-container"
+
         className="flex flex-col p-2 tracking-wide">
         <h1
           id="organization-header"
