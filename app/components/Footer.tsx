f76ad842341b5060d2f20e32a14a1df9887bcbe9
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-06 18:51:46 -0800

    Re-adjusted formatting and testing for error line resolution, attempt 11

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,11 +11,11 @@ export default function Footer() {
       id="container"
         className="flex justify-around bg-[#0076AD] text-white pt-8 pb-11">
       <div
-      id="footer-left-content-container"
-        className="flex flex-col p-2 tracking-wide">
+        id="footer-left-content-container"
+          className="flex flex-col p-2 tracking-wide">
         <h1
-        id="organization-header"
-          className="text-lg font-semibold tracking-wide text-center">
+          id="organization-header"
+            className="text-lg font-semibold tracking-wide text-center">
           LuskinOIC Pediatric Orthopedic
         </h1>
         <div className="text-sm font-medium">
