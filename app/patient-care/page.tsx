e5dca28dc92425b21e4a591b854c43ad16030518
Spencer Ward <swkineo@gmail.com>
2023-11-08 18:08:31 -0500

    Add shadcn example component to the Patient Care page

--- app/patient-care/page.tsx
+++ app/patient-care/page.tsx
@@ -1,13 +1,16 @@
+import TabbedSection from "@/app/components/TabbedSection";
+
 export default async function PatientCare() {
   // const patientCarePage = await getPatientCarePage();
 
   return (
     <main>
-      <div className="md:grid md:grid-cols-3">
+      {/* <div className="md:grid md:grid-cols-3">
         <div className="grid-span-1">column 1</div>
         <div className="grid-span-2">column 1</div>
         <div className="grid-span-1">column 1</div>
-      </div>
+      </div> */}
+      <TabbedSection />
     </main>
   );
 }
