e14aea05696e961f4486a89f0f730a42cc3fa8e6
Spencer Ward <swkineo@gmail.com>
2023-11-08 18:33:26 -0500

    Convert shadcn example to empty structure

--- app/components/TabbedSection.tsx
+++ app/components/TabbedSection.tsx
@@ -15,60 +15,28 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs";
+import { TabbedSectionType } from "../constants/types";
 
-export default function TabbedSection() {
+export default function TabbedSection({
+  keys,
+  labels,
+  contents
+}: TabbedSectionType) {
   return (
     <Tabs defaultValue="account" className="w-[400px]">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
+        {keys.map((key: string, index: number) => (
+          <TabsTrigger value={key}>{labels[index]}</TabsTrigger>
+        ))}
+        <TabsTrigger value="planning">Plan Your Visit</TabsTrigger>
+        <TabsTrigger value="billing">Billing & Insurance</TabsTrigger>
+        <TabsTrigger value="rights">Patient Rights & Responsibilities</TabsTrigger>
+        <TabsTrigger value="prep">Appointment Preparation</TabsTrigger>
+        <TabsTrigger value=""></TabsTrigger>
       </TabsList>
-      <TabsContent value="account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
+      {keys.map((key: string, index: number) => (
+        <TabsContent value={key}>{contents[index]}</TabsContent>
+      ))}
     </Tabs>
   );
 }
--- app/constants/types.ts
+++ app/constants/types.ts
@@ -73,3 +73,9 @@ export type LandingPageType = {
   goalSection: PageSectionType;
   sections: PageSectionType[];
 };
+
+export type TabbedSectionType = {
+  keys: string[],
+  labels: string[],
+  contents: React.ReactNode[]
+}
