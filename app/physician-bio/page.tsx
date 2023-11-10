6d1d817653fd3e7d19651061d12b5ceb0a817fc6
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-10 15:28:53 -0800

    Added secondary sample data in Physician Bio data model

--- app/physician-bio/page.tsx
+++ app/physician-bio/page.tsx
@@ -1,13 +1,16 @@
 import PhysicianBio from './[slug]/page'
 
 export default function PhysicianBiography() {
-  const params = {
-    slug: 'anthony-scaduto-md'
-  }
+  // const params = {
+  //   slug: 'anthony-scaduto-md',
+  // }
+  const physicianSlugs = ['anthony-scaduto-md', 'doctor-md']
   return (
     <main>
       <h1>Physician Biography</h1>
-      <PhysicianBio params={params}/>
+      {physicianSlugs.map((slug) => (
+        <PhysicianBio key={slug} params={{ slug }} />
+      ))} 
     </main>
   );
 }
