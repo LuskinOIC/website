a10e27300bf8692c39e5459dac6bfa7be43103c4
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-10 16:03:15 -0800

    Formatting to pass linting, attempt 5

--- app/physician-bio/[slug]/page.tsx
+++ app/physician-bio/[slug]/page.tsx
@@ -8,9 +8,9 @@ export default async function PhysicianBio({
   params: { slug: string };
 }) {
   const docBio = await getPhysicianBioBySlug(params.slug);
-  console.log("DOC BIO", docBio);
-  console.log("SLUG", docBio.slug);
-  console.log("PHYSICIAN NAME", docBio.physicianName);
+  // console.log("DOC BIO", docBio);
+  // console.log("SLUG", docBio.slug);
+  // console.log("PHYSICIAN NAME", docBio.physicianName);
   return (
     <main>
       <h1>Physician Bio - {docBio.physicianName}</h1>
