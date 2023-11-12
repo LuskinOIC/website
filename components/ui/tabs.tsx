9edbc364b34c305b565110555a498d6f66aacc2e
Spencer Ward <swkineo@gmail.com>
2023-11-12 18:45:52 -0500

    Update tab colors

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -41,7 +41,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-[88px] w-[1050px] pt-[24px] items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
+      "inline-flex h-[88px] w-[1050px] pt-[24px] items-center justify-center rounded-md bg-white p-1 text-muted-foreground",
       className
     )}
     {...props}
@@ -56,7 +56,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      `inline-flex items-center justify-center whitespace-nowrap rounded-t-lg px-[30px] py-[16px] text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[hsla(214, 25%, 49%, 0.15))] data-[state=active]:bg-[] data-[state=active]:text-foreground`,
+      `inline-flex items-center justify-center whitespace-nowrap rounded-t-lg px-[30px] py-[16px] text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-slate-200 data-[state=active]:bg-[#5F7A9D] data-[state=active]:text-foreground`,
       // "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
       className
     )}
