6028840c5888521793e1af110de5e797e250c346
Spencer Ward <swkineo@gmail.com>
2023-11-14 11:52:41 -0500

    Appease linter for shadcn code (pass 2)

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -30,26 +30,26 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
-};
+  asChild?: boolean;
+}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
-  }
+    );
+  },
 );
 Button.displayName = "Button";
 
