e86062755facd1bfdc9f949c044f3bc8fc0479b2
Yoline <92892101+YolineB@users.noreply.github.com>
2023-11-14 14:20:10 -0500

    Correct headers

--- app/components/PageSection/InfoCardLayout.tsx
+++ app/components/PageSection/InfoCardLayout.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 // Types
 import { PageSectionType } from "@/app/constants/types";
-import { Title2 } from "../ui/Typography/Title";
 import { Text2 } from "../ui/Typography/Text";
+import { Title2 } from "../ui/Typography/Title";
 
 export default function InfoCardLayout({
   section,
@@ -12,12 +12,12 @@ export default function InfoCardLayout({
   section: PageSectionType;
 }) {
   return (
-    <section className="grid md:grid-cols-2 gap-3 m-auto md:w-3/4 px-10 py-8">
+    <section className="grid md:grid-cols-2 gap-3 m-auto md:w-3/4 px-2 py-8">
       {section.fields.infoCards.map((card, i) => {
         return (
           <div
             key={i}
-            className="col-span-1 rounded-[10px] shadow border border-black border-opacity-10 pb-8 md:p-6 overflow-hidden"
+            className="col-span-1 rounded-[10px] shadow border border-black border-opacity-10 pb-8 md:p-10 overflow-hidden"
           >
             <div className="relative md:rounded-[10px] overflow-hidden">
               <Image
@@ -30,14 +30,12 @@ export default function InfoCardLayout({
                 height={400}
               />
             </div>
-            <div className="md:h-min-[242px] px-10 md:px-0 flex-col ">
+            <div className="md:h-min-[242px] px-5 md:px-0 flex-col ">
               <div className="font-arial leading-150 py-4 ">
-                <Title2 className="font-bold">{card.fields.title}</Title2>
-                <Text2 className="text-xl">{card.fields.subTitle}</Text2>
+                <Title2>{card.fields.title}</Title2>
+                <Text2>{card.fields.subTitle}</Text2>
               </div>
-              <Text2 className="text-xl font-normal leading-[30px] py-4">
-                {card.fields.content}
-              </Text2>
+              <Text2>{card.fields.content}</Text2>
             </div>
           </div>
         );
--- app/components/ui/Typography/Title.tsx
+++ app/components/ui/Typography/Title.tsx
@@ -14,7 +14,7 @@ export const Title1 = ({ children, className = "" }: TitleProps) => {
 };
 
 export const Title2 = ({ children, className = "" }: TitleProps) => {
-  const classes = `text-xl font-arial leading-150 py-2 ${className}`;
+  const classes = `text-xl md:text-2xl font-arial leading-150 py-2 ${className}`;
   return <h1 className={classes}>{children}</h1>;
 };
 export const Title3 = ({ children, className = "" }: TitleProps) => {
