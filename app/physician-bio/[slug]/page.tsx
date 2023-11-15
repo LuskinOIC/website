1e6538d513ef7c636d218843cc61feae42177d5d
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-14 16:05:00 -0800

    Edits to Types and Contentful for data model Physician Bio

--- app/physician-bio/[slug]/page.tsx
+++ app/physician-bio/[slug]/page.tsx
@@ -1,5 +1,24 @@
-export function generateStaticParams() {
-  return [{ slug: "place-holder" }];
+import { getPhysicianBio, getPhysicianBioBySlug } from "@/app/utils/contentful";
+
+// export async function generateStaticParams() {
+//   const physicians = await getPhysicianBioBySlug('anthony-scaduto-md');
+//   // temp hard code
+
+//   if (physicians.fields && physicians.fields.slug) {
+//     console.log(physicians.fields.slug);
+//     const slug = physicians.fields.slug;
+//     return[{ slug }]
+//   }
+//   console.log('HELLO WORLD', physicians.fields)
+//   return [];
+// }
+interface SpecialtyType {
+  value: string;
+}
+
+export async function generateStaticParams() {
+  const physicians = await getPhysicianBio();
+  return physicians.map((evt) => ({ slug: evt.slug }));
 }
 
 export default async function PhysicianBio({
@@ -8,12 +27,19 @@ export default async function PhysicianBio({
   params: { slug: string };
 }) {
   const docBio = await getPhysicianBioBySlug(params.slug);
-  // console.log("DOC BIO", docBio);
+  console.log("DOC BIO", docBio);
+  console.log('SPECIALTIES', docBio.specialties.content)
   // console.log("SLUG", docBio.slug);
-  console.log("PHYSICIAN NAME", docBio.physicianName);
+  // console.log("PHYSICIAN NAME", docBio.physicianName);
   return (
     <main>
       <h1>Physician Bio - {docBio.physicianName}</h1>
+      <h4>Specialties</h4>
+      <p>Specializes in: {docBio.specialties?.content.map((specialty: {value: string }, index: number) => (
+        <p key={index}>{specialty.value}</p>
+      ))}</p>
+      <p>{docBio.appointmentNumber}</p>
+      <p>{docBio.physicianNumber}</p>
     </main>
   );
 }
--- app/utils/contentful.ts
+++ app/utils/contentful.ts
@@ -53,6 +53,7 @@ export async function getSocialMediaSection(): Promise<SocialMediaSectionType> {
   return entry.fields as SocialMediaSectionType;
 }
 
+
 export async function getPhysicianBio() {
   const entries = await client.getEntries({
     content_type: "physicianBio",
