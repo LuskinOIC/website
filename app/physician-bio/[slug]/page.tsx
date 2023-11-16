f41ddf03637b3bd033191af943ca4fe612abe07e
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-15 18:13:04 -0800

    Included code for rich-text rendering

--- app/physician-bio/[slug]/page.tsx
+++ app/physician-bio/[slug]/page.tsx
@@ -2,30 +2,25 @@ import { getPhysicianBio, getPhysicianBioBySlug } from "@/app/utils/contentful";
 import { BLOCKS, MARKS, DOCUMENT } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-export async function generateStaticParams() {
-  const physicians = await getPhysicianBio();
-  return physicians.map((evt) => ({ slug: evt.slug }));
+const Bold = ({ children }: { children: React.ReactNode }) => (
+  <span className="font-bold">{children}</span>
+);
+const Text = ({ children }: { children: React.ReactNode }) => (
+  <p className="align-center">{children}</p>
+);
+const List = ({ children }: { children: React.ReactNode }) => (
+  <li className="font-bold">{children}</li>
+);
+
+function renderRichTextToReactComponent(richText: Document, options = {}) {
+  const defaultOptions = {
+    renderNode: {
+      [BLOCKS.LIST_ITEM]: (node: any, children: React.ReactNode) => (
+        <li>{children}</li>
+      ),
+    },
+  };
 }
-
-// const Bold = ({ children }) => <span className="font-bold">{children}</span>;
-
-// const Text = ({ children }) => <p className="align-center">{children}</p>;
-
-// const List = ({ children }) => <li className="font-bold">{children}</li>;
-
-// const optdefaultOptionsions = {
-//   // renderMark: {
-//   //   [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
-//   // },
-//   // renderNode: {
-//   //   [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
-//   // },
-//   renderNode: {
-//     [BLOCKS.LIST_ITEM]: (node, children) => <List>{children}</List>,
-//   },
-// };
-
-// function renderRichTextToReactComponent(richText, options = {});
 export default async function PhysicianBio({
   params,
 }: {
