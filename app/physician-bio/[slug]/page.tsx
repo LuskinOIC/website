3a12168b60ef34787db06356b0016ffebfca9cd6
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-15 18:15:22 -0800

    Replaced general code from rich-text.tsx with code tailored for Physician Bio

--- app/physician-bio/[slug]/page.tsx
+++ app/physician-bio/[slug]/page.tsx
@@ -1,13 +1,14 @@
 import { getPhysicianBio, getPhysicianBioBySlug } from "@/app/utils/contentful";
-import { BLOCKS, MARKS, DOCUMENT } from "@contentful/rich-text-types";
+import { BLOCKS, MARKS, Document } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-const Bold = ({ children }: { children: React.ReactNode }) => (
-  <span className="font-bold">{children}</span>
-);
-const Text = ({ children }: { children: React.ReactNode }) => (
-  <p className="align-center">{children}</p>
-);
+export async function generateStaticParams() {
+  const physicians = await getPhysicianBio();
+  return physicians.map((evt) => ({ slug: evt.slug }));
+}
+
+// const Bold = ({ children }) => <span className="font-bold">{children}</span>;
+// const Text = ({ children }) => <p className="align-center">{children}</p>;
 const List = ({ children }: { children: React.ReactNode }) => (
   <li className="font-bold">{children}</li>
 );
@@ -16,11 +17,14 @@ function renderRichTextToReactComponent(richText: Document, options = {}) {
   const defaultOptions = {
     renderNode: {
       [BLOCKS.LIST_ITEM]: (node: any, children: React.ReactNode) => (
-        <li>{children}</li>
+        <li className="font-bold">{children}</li>
       ),
     },
   };
+
+  return documentToReactComponents(richText, { ...defaultOptions, ...options });
 }
+
 export default async function PhysicianBio({
   params,
 }: {
@@ -36,7 +40,7 @@ export default async function PhysicianBio({
       </h1>
       <h4 className="text-lg text-[#800080]">Specialties</h4>
       <p className="text-[#800080]"></p>
-      {documentToReactComponents(docBio.specialties, options)}
+      {renderRichTextToReactComponent(docBio.specialties as Document)}
       <p>Appointment Number: {docBio.appointmentNumber}</p>
       <p>Physician Number: {docBio.physicianNumber}</p>
     </main>
