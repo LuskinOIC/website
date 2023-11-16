16659adc74b61d9aa29dbf48f22a32c1ebd50f70
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-16 14:34:23 -0800

    Adjustments to rich-text.tsx

--- app/utils/rich-text.tsx
+++ app/utils/rich-text.tsx
@@ -1,22 +1,19 @@
-import { BLOCKS, MARKS, Document } from "@contentful/rich-text-types";
+import { BLOCKS, Document } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-const Bold = ({ children }: { children: React.ReactNode }) => (
-  <span className="font-bold">{children}</span>
-);
-const Text = ({ children }: { children: React.ReactNode }) => (
-  <p className="align-center">{children}</p>
-);
-const List = ({ children }: { children: React.ReactNode }) => (
-  <li className="list-disc">{children}</li>
-);
 
 function renderRichTextToReactComponent(richText: Document, options = {}) {
   const defaultOptions = {
     renderNode: {
       [BLOCKS.LIST_ITEM]: (node: any, children: React.ReactNode) => (
-        <li>{children}</li>
+        <li className="list-disc">{children}</li>
+      ),
+      [BLOCKS.PARAGRAPH]: (node: any, children: React.ReactNode) => (
+        <p>{children}</p>
       ),
     },
   };
+  return documentToReactComponents(richText, {...defaultOptions, ...options});
 }
+
+// more examples: https://github.com/contentful/rich-text/blob/master/packages/rich-text-react-renderer/src/index.tsx
