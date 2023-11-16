e8a050a170374deef8f7a2eee2c2c840ba0f6212
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-15 18:57:37 -0800

    Added bullet points to list items

--- app/physician-bio/[slug]/page.tsx
+++ app/physician-bio/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { getPhysicianBio, getPhysicianBioBySlug } from "@/app/utils/contentful";
 import { BLOCKS, MARKS, Document } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
@@ -17,7 +18,7 @@ function renderRichTextToReactComponent(richText: Document, options = {}) {
   const defaultOptions = {
     renderNode: {
       [BLOCKS.LIST_ITEM]: (node: any, children: React.ReactNode) => (
-        <li className="font-bold">{children}</li>
+        <li className="font-bold list-disc">{children}</li>
       ),
     },
   };
@@ -34,7 +35,7 @@ export default async function PhysicianBio({
   const firstObject = docBio.specialties;
 
   return (
-    <main>
+    <main className="m-10">
       <h1 className="text-lg text-[#800080]">
         Physician Bio - {docBio.physicianName}
       </h1>
