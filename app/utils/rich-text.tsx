98488d57d2862afec0fea88ec1c335d8b8e9e67a
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-15 18:08:17 -0800

    Added rich-text file to utils folder

---
+++ app/utils/rich-text.tsx
@@ -0,0 +1,22 @@
+import { BLOCKS, MARKS, Document } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const Bold = ({ children }: { children: React.ReactNode }) => (
+  <span className="font-bold">{children}</span>
+);
+const Text = ({ children }: { children: React.ReactNode }) => (
+  <p className="align-center">{children}</p>
+);
+const List = ({ children }: { children: React.ReactNode }) => (
+  <li className="font-bold">{children}</li>
+);
+
+function renderRichTextToReactComponent(richText: Document, options = {}) {
+  const defaultOptions = {
+    renderNode: {
+      [BLOCKS.LIST_ITEM]: (node: any, children: React.ReactNode) => (
+        <li>{children}</li>
+      ),
+    },
+  };
+}
