67091759ecfc34c0ead9a43e168697663c3c8fb2
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-17 18:58:35 -0800

    Re-reverted code in InfoCardLayout

--- app/physician-bio/[slug]/page.tsx
+++ app/physician-bio/[slug]/page.tsx
@@ -41,10 +41,10 @@ const Section = ({ section }: { section: BioPageSectionType }) => (
       {section.fields.content.content.map(
         (
           contentItem: { content: Array<{ value: string }> },
-          contentIndex: number
+          contentIndex: number,
         ) => (
           <ContentParagraph key={contentIndex} contentItem={contentItem} />
-        )
+        ),
       )}
     </div>
   </div>
@@ -72,7 +72,7 @@ export default async function PhysicianBio({
         <h3 className="text-base">Specializes in:</h3>
         <div>
           {renderRichTextToReactComponent(
-            docBio.specialties as unknown as Document
+            docBio.specialties as unknown as Document,
           )}
         </div>
         <div id="phone-numbers">
