6ea6dd910e607272bbb3df201222fe0eb274e41f
Yoline <92892101+YolineB@users.noreply.github.com>
2023-11-17 20:27:38 -0800

    Correct background color format

--- app/components/PageSection/TwoColumnLayout.tsx
+++ app/components/PageSection/TwoColumnLayout.tsx
@@ -3,15 +3,9 @@ import Image from "next/image";
 // Types
 import { PageSectionType } from "@/app/constants/types";
 // Custom components
-import { TitleComponent } from "@/app/components/ui/Typography/Title";
+import { Title1 } from "@/app/components/ui/Typography/Title";
 import { Text1 } from "../ui/Typography/Text";
 import Button from "../ui/Button";
-
-const styles = {
-  pageSectionContent: "grid gap-2",
-  actionButton:
-    "block mx-auto text-center w-full bg-blue-500 rounded text-white p-2",
-};
 
 export default function ColumnLayout({
   section,
@@ -19,13 +13,17 @@ export default function ColumnLayout({
   section: PageSectionType;
 }) {
   const orderClass = section.fields.reverseOrder ? "md:order-1" : "";
+  const bgColor = `bg-luskin-${section.fields.backgroundColor}`;
+  const textColor =
+    section.fields.backgroundColor !== "white" ? "text-white" : "text-black";
 
   return (
     <section
-      className={`grid md:grid-cols-2 md:gap-14 px-6 md:px-0 py-3 md:py-10`}
+      className={`${bgColor} grid md:grid-cols-2 md:gap-14 px-6 md:px-0 py-3 md:py-0`}
     >
+      {/* TODO: Adjust image sizing */}
       {section.fields.image && (
-        <div className={`col-span-1 ${orderClass} flex justify-center h-auto`}>
+        <div className={`col-span-1 ${orderClass} flex justify-center h-fit`}>
           <Image
             className="fill-inherit"
             src={`https:${section.fields.image.fields.file.url}`}
@@ -40,18 +38,13 @@ export default function ColumnLayout({
           />
         </div>
       )}
-
+      {/* TODO: Adjust letter spacing, + Patient Care header order */}
       <div className="col-span-1 grid gap-2 md:px-40 py-9 md:py-40">
-        <div className={styles.pageSectionContent}>
-          <TitleComponent
-            title={section.fields.title}
-            bold={section.fields.bold}
-            titleSize="Large"
-          />
+        <div className={`grid gap-2 ${textColor}`}>
+          <Title1>{section.fields.title}</Title1>
           <Text1>{section.fields.description}</Text1>
-          <Text1>{section.fields.backgroundColor}</Text1>
         </div>
-
+          {/* TODO: Provide text button option */}
         <div className="grid md:grid-cols-2 gap-4 md:gap-6 pt-5">
           {section.fields.actionUrl && (
             <Button
