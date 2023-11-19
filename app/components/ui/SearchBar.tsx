fdffd1ac757ebfde25fc8308d49e115db2f9a529
Joe Torreggiani <joetorreggiani@gmail.com>
2023-11-19 18:31:20 -0500

    Make linter happy.

--- app/components/ui/SearchBar.tsx
+++ app/components/ui/SearchBar.tsx
@@ -7,12 +7,10 @@ export default function SearchBar() {
           placeholder="Search..."
           className="pl-4 pr-20 py-2 rounded-l-md border border-r-0 w-full"
         />
-        <button
-          className="absolute right-0 top-0 px-4 py-2 bg-blue-500 text-white rounded-r-md border border-blue-500"
-        >
+        <button className="absolute right-0 top-0 px-4 py-2 bg-blue-500 text-white rounded-r-md border border-blue-500">
           Search
         </button>
       </div>
     </div>
-  )
+  );
 }
--- app/physicians/page.tsx
+++ app/physicians/page.tsx
@@ -1,25 +1,32 @@
 import { getPhysicians } from "@/app/utils/contentful";
 import SearchBar from "@/app/components/ui/SearchBar";
 import { PhysicianBioType } from "@/app/constants/types";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default async function Physicians() {
-  const physicians = await getPhysicians() as PhysicianBioType[];
+  const physicians = (await getPhysicians()) as PhysicianBioType[];
 
   return (
     <main>
       <SearchBar />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4"> 
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
         {physicians.map((physician) => (
-          <div key={physician.physicianName} className="border rounded-lg p-4 shadow-md flex">
+          <div
+            key={physician.physicianName}
+            className="border rounded-lg p-4 shadow-md flex"
+          >
             <div className="flex-none w-1/3 mr-4">
-            <Image
-              src={physician.physicianPortrait.fields.file.url}
-              alt=""
-              width={physician.physicianPortrait.fields.file.details.image.width}
-              height={physician.physicianPortrait.fields.file.details.image.height}
-            />
+              <Image
+                src={physician.physicianPortrait.fields.file.url}
+                alt=""
+                width={
+                  physician.physicianPortrait.fields.file.details.image.width
+                }
+                height={
+                  physician.physicianPortrait.fields.file.details.image.height
+                }
+              />
             </div>
             <div className="flex-grow">
               <h2 className="text-lg font-bold">{physician.physicianName}</h2>
