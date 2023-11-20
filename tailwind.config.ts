de3fc8a1b6c880fb0a450372eba42422854c6f7e
Joe Torreggiani <joetorreggiani@gmail.com>
2023-11-19 20:01:49 -0500

    Make ts build happy.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,19 +66,18 @@ const config: Config = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: '0' },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: '0' },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-
     },
   },
   plugins: [require("tailwindcss-animate")],
