ef5fc6b4e3e1b4104337d3c61ded578271724926
Khiry <khiryshank@gmail.com>
2023-11-19 16:48:26 -0800

    creating new branch/adding temp card

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import SocialMediaBanner from "@/app/components/SocialMediaBanner";
+import QuadCard from "@/app/components/QuadCard";
+
 
 export default function About() {
   return (
     <main>
       <h1>TOGETHER, we are LuskinOIC.</h1>
       <SocialMediaBanner />
+      <QuadCard />
     </main>
   );
 }
