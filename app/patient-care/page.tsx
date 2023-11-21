379411e3b31c00e46faa7a7697758a7b653641d9
Spencer Ward <swkineo@gmail.com>
2023-11-21 12:59:22 -0500

    Replace Patient Care hardcoding with dynamic content (and temporary placeholder code)

--- app/patient-care/page.tsx
+++ app/patient-care/page.tsx
@@ -2,6 +2,7 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { getPatientCarePage } from "@/app/utils/contentful";
 import TabsTextOrCardContent from "../components/TabsTextOrCardContent";
 
+/* Here to next comment can be removed once values are in Contentful */
 const placeholderCard = {
   header: "Header",
   body: "Considerate and respectful care, and to be made comfortable. You have the right to respect for your cultural, psychosocial, spiritual, and personal values, beliefs and preferences. Have family (or other representative of your choosing) be involved in care, treatment, or services decisions to the extent permitted by you or your surrogate decision maker, in accordance with laws and regulations.   Know the name of the licensed health care practitioner acting within the scope of his or her professional licensure who has primary relationship for coordinating you",
@@ -9,65 +10,69 @@ const placeholderCard = {
   buttonText: "Button",
 };
 
-const placeHolderFields = {
-  tabTitle: "",
-  cardContent: [
-    placeholderCard,
-    placeholderCard,
-    placeholderCard,
-    placeholderCard,
-    placeholderCard,
-    placeholderCard,
-  ],
-};
+const placeholderCardContent = [
+  placeholderCard,
+  placeholderCard,
+  placeholderCard,
+  placeholderCard,
+  placeholderCard,
+  placeholderCard,
+];
+/* */
 
 export default async function PatientCare() {
   const patientCarePage = await getPatientCarePage();
 
+  /* Here to next comment can be removed once values are in Contentful */
+  if (patientCarePage.tabSection.fields.tabs.length == 2) {
+    patientCarePage.tabSection.fields.tabs.push(
+      {
+        fields: {
+          tabTitle: "Patient Rights & Responsibilities",
+          cardContent: placeholderCardContent,
+        },
+      },
+      {
+        fields: {
+          tabTitle: "Appointment Preparation",
+          cardContent: placeholderCardContent,
+        },
+      },
+      {
+        fields: {
+          tabTitle: "Patient Forms",
+          cardContent: placeholderCardContent,
+        },
+      },
+    );
+  }
+  /* */
+
   return (
     <main>
-      <Tabs defaultValue="planning">
-        <TabsList className="grid grid-cols-5">
-          <TabsTrigger value="planning" className="ml-0">
-            <h1>
-              {/* NOTE we will be able to iterate instead of hardcoding */}
-              {patientCarePage.tabSection.fields.tabs[0].fields.tabTitle}
-            </h1>
-          </TabsTrigger>
-          <TabsTrigger value="billing">
-            <h1>{patientCarePage.tabSection.fields.tabs[1].fields.tabTitle}</h1>
-          </TabsTrigger>
-          <TabsTrigger value="rights">
-            <h1>PATIENT RIGHTS &</h1>
-            <h1>RESPONSIBILITIES</h1>
-          </TabsTrigger>
-          <TabsTrigger value="prep">
-            <h1>APPOINTMENT</h1>
-            <h1>PREPARATION</h1>
-          </TabsTrigger>
-          <TabsTrigger value="forms" className="mr-0">
-            <h1>PATIENT FORMS</h1>
-          </TabsTrigger>
+      <Tabs
+        defaultValue={patientCarePage.tabSection.fields.tabs[0].fields.tabTitle}
+      >
+        <TabsList
+          className={
+            "grid grid-cols-" + patientCarePage.tabSection.fields.tabs.length
+          }
+        >
+          {patientCarePage.tabSection.fields.tabs.map((tab, index) => (
+            <TabsTrigger
+              key={index}
+              value={tab.fields.tabTitle}
+              className="ml-0"
+            >
+              <h1 className="uppercase">{tab.fields.tabTitle}</h1>
+            </TabsTrigger>
+          ))}
         </TabsList>
-        <TabsContent value="planning">
-          <TabsTextOrCardContent
-            fields={patientCarePage.tabSection.fields.tabs[0].fields}
-          />
-        </TabsContent>
-        <TabsContent value="billing">
-          <TabsTextOrCardContent
-            fields={patientCarePage.tabSection.fields.tabs[1].fields}
-          />
-        </TabsContent>
-        <TabsContent value="rights">
-          <TabsTextOrCardContent fields={placeHolderFields} />
-        </TabsContent>
-        <TabsContent value="prep">
-          <TabsTextOrCardContent fields={placeHolderFields} />
-        </TabsContent>
-        <TabsContent value="forms">
-          <TabsTextOrCardContent fields={placeHolderFields} />
-        </TabsContent>
+        {patientCarePage.tabSection.fields.tabs.map((tab, index) => (
+          <TabsContent key={index} value={tab.fields.tabTitle}>
+            <TabsTextOrCardContent fields={tab.fields} />
+          </TabsContent>
+        ))}
       </Tabs>
     </main>
   );
