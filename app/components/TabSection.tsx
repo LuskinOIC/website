4cb0be877001b8d856b6b6b6c98312aee297b0bf
Spencer Ward <swkineo@gmail.com>
2023-11-29 15:13:21 -0500

    Convert mobile TabSection to navigate with anchors

--- app/components/TabSection.tsx
+++ app/components/TabSection.tsx
@@ -1,4 +1,3 @@
-// TODO: Change hardcoded grid-cols-5 to grid-cols-${tabCount} in styles.tabsList
 "use client";
 
 import { TabSectionType, TabType } from "../constants/types";
@@ -21,10 +20,11 @@ import {
 import { useState } from "react";
 import Image from "next/image";
 import { Title3 } from "./ui/Typography/Title";
+import { useRouter } from "next/navigation";
 
 const styles = {
-  tabsList: (/* tabCount: number */) =>
-    `grid grid-cols-5 h-16 min-w-fit w-[65rem] mt-[24px] mb-10 justify-items-stretch rounded-md bg-white border-b-[0.188rem] border-[#5F7A9D] p-[0]`,
+  tabsList: (tabCount: number) =>
+    `grid grid-cols-${tabCount} h-16 min-w-fit w-[65rem] mt-[24px] mb-10 justify-items-stretch rounded-md bg-white border-b-[0.188rem] border-[#5F7A9D] p-[0]`,
   tabsTrigger: (index: number, tabCount: number) => {
     // Set left margin of first item and right margin of last item to 0
     let horizontalMargin: string = "";
@@ -70,7 +70,8 @@ export default function TabSection({ fields: { tabs } }: TabSectionType) {
 
 type DesktopTabSectionType = {
   styles: {
-    tabsList: (/* tabCount: number */) => string;
+    // eslint-disable-next-line no-unused-vars
+    tabsList: (tabCount: number) => string;
     // eslint-disable-next-line no-unused-vars
     tabsTrigger: (index: number, tabCount: number) => string;
   };
@@ -94,7 +95,7 @@ function DesktopTabSection({
         value={selectedTab}
         className="flex flex-col items-center w-full h-fit mb-12"
       >
-        <TabsList className={styles.tabsList(/* tabCount */)}>
+        <TabsList className={styles.tabsList(tabCount)}>
           {tabs.map((tab, index) => (
             <TabsTrigger
               key={index}
@@ -131,40 +132,52 @@ function MobileTabSection({
   selectedTab,
   setSelectedTab,
 }: MobileTabSectionType) {
+  const router = useRouter();
+
   return (
     /* Only show on mobile */
-    <div className="flex md:hidden flex-col items-center w-full mt-6">
-      <DropdownMenu>
-        <DropdownMenuTrigger className="w-[85vw] h-10 pl-5 border border-luskin-blue rounded-md">
-          <div className="flex flex-row w-full">
-            <Title3 className="grow self-start text-start align-middle uppercase text-sm text-gray-500">
-              {selectedTab}
-            </Title3>
-            <Image
-              src="/dropdownarrow.svg"
-              width={20}
-              height={10}
-              alt=""
-              className="justify-self-end self-center mr-4"
-            />
-          </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-[85vw]">
-          {tabs.map((tab, index) => (
-            <DropdownMenuItem
-              key={index}
-              onSelect={() => setSelectedTab(tab.fields.tabTitle)}
-            >
-              <Title3 className="uppercase text-luskin-blue font-bold">
-                {tab.fields.tabTitle}
+    <div className="relative flex md:hidden flex-col items-center w-full mt-6">
+      <div className="sticky top-0 flex flex-col items-center w-full pt-3 pb-1 bg-white">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="w-[85vw] h-10 pl-5 border border-luskin-blue rounded-md">
+            <div className="flex flex-row w-full">
+              <Title3 className="grow self-start text-start align-middle uppercase text-sm text-gray-500">
+                {selectedTab}
               </Title3>
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
+              <Image
+                src="/dropdownarrow.svg"
+                width={20}
+                height={10}
+                alt=""
+                className="justify-self-end self-center mr-4"
+              />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[85vw]">
+            {tabs.map((tab, index) => (
+              <DropdownMenuItem
+                key={index}
+                onSelect={() => {
+                  setSelectedTab(tab.fields.tabTitle);
+                  router.push(
+                    `/patient-care#${tab.fields.tabTitle.replace(
+                      /[^A-z]/g,
+                      "",
+                    )}`,
+                  );
+                }}
+              >
+                <Title3 className="uppercase text-luskin-blue font-bold">
+                  {tab.fields.tabTitle}
+                </Title3>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <div className="pt-10 w-4/5">
         {tabs.map((tab, index) => (
-          <div key={index}>
+          <div id={tab.fields.tabTitle.replace(/[^A-z]/g, "")} key={index}>
             <Title3
               key={index}
               className="mt-3 uppercase text-luskin-blue font-medium"
