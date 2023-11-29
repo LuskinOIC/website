4d597698bb65b74042b6ff93228dcb3a3047ec88
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-11-29 14:46:16 -0800

    Added imports for getEvents and EventType from contentful, types respectively

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,6 +1,11 @@
 import { getEvents } from "@/app/utils/contentful"
+import { EventType } from "@/app/constants/types"
+import Image from "next/image"
+import Link from "next/link"
 
-export default function Events() {
+export default async function Events() {
+
+  const events = (await getEvents()) as unknown as EventType[];
   return (
     <main>
       <h1>LuskinOIC&apos;s Leadership Drives Our Mission Forward</h1>
