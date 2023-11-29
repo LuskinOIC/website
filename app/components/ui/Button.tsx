94196f1f13927f2f5a905dd9d8259d7fd614954b
Spencer Ward <swkineo@gmail.com>
2023-11-29 14:15:52 -0500

    Add tailwind-merge to Button component

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -4,9 +4,13 @@ import { Text2 } from "./Typography/Text";
 
 // Types
 import { ButtonProps } from "@/app/constants/types";
+import { cn } from "@/lib/utils";
 
 const Button = ({ href, text, className = "" }: ButtonProps) => {
-  const classes = `flex w-fit h-[43px] md:h-[53px] lg:h-[63px] px-6 py-3 bg-[#0076AD] rounded-[10px] items-center text-white md:font-bold ${className}`;
+  const classes = cn(
+    "flex w-fit h-[43px] md:h-[53px] lg:h-[63px] px-6 py-3 bg-[#0076AD] rounded-[10px] items-center text-white md:font-bold",
+    className,
+  );
   return (
     <Link className={classes} href={href}>
       <Text2>{text}</Text2>
