e7a6700a075ad84b6573ecf5332d9627b9ff6aaf
Khiry <khiryshank@gmail.com>
2023-12-01 15:04:11 -0800

    Adding legacy cards

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SocialMediaBanner from "@/app/components/SocialMediaBanner";
+import LegacyCard from "../components/LegacyCard";
 
 export default function About() {
   return (
     <main>
       <h1>TOGETHER, we are LuskinOIC.</h1>
       <SocialMediaBanner />
+      <LegacyCard />
     </main>
   );
 }
