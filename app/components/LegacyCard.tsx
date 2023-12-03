41a87f09b1556afbecd317ad414626ff42102df8
Khiry <khiryshank@gmail.com>
2023-12-02 17:56:29 -0800

    Added email and phone icons

--- app/components/LegacyCard.tsx
+++ app/components/LegacyCard.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Image from "next/image";
+import phone from "@/public/phone.svg";
+import email from "@/public/email.png"
+
 
 export default function LegacyCard() {
   return (
-    <div className=" grid grid-cols-2 gap-4">
+    <div className=" grid md:grid-cols-2 sm:grid-cols-1  gap-4">
       <div className="bg-[#FFF2C0] rounded p-8">
         <h1>
           <b>Sponsor an Event</b>
@@ -12,6 +16,13 @@ export default function LegacyCard() {
           children and promote your company or organization.
         </p>
         <p>To find out more contact: Mary Beth Perrine</p>
+        <Image
+                src={phone}
+                alt="phone"
+                height={20}
+                width={20}
+                
+              />
       </div>
       <div className="bg-[#FFF2C0] rounded p-8">
         <h1>
@@ -22,7 +33,15 @@ export default function LegacyCard() {
           mission.
         </p>
         <p>To find out more contact: Mary Beth Perrine</p>
+        <Image
+                src={phone}
+                alt="phone"
+                height={20}
+                width={20}
+                
+              />
       </div>
+           
       <div className="bg-[#FFF2C0] rounded p-8">
         <h1>
           <b>Naming Opportunities</b>
@@ -33,6 +52,13 @@ export default function LegacyCard() {
           the perfect fit for you.
         </p>
         <p>To find out more contact: Mary Beth Perrine</p>
+        <Image
+                src={phone}
+                alt="phone"
+                height={20}
+                width={20}
+                
+              />
       </div>
       <div className="bg-[#FFF2C0] rounded p-8">
         <h1>
@@ -44,6 +70,26 @@ export default function LegacyCard() {
           recognized in our legacy society.
         </p>
         <p>To find out more contact: Mary Beth Perrine</p>
+        <div className="flex space-x-4">
+        <Image
+                src={phone}
+                alt="phone"
+                height={20}
+                width={20}
+                
+              /><span>(213) 742-1500</span>
+        
+            </div>
+        <div className="flex space-x-4">
+        <Image
+                src={email}
+                alt="phone"
+                height={20}
+                width={20}
+                
+              />
+          <span>blah@gmail.com</span>
+        </div>
       </div>
     </div>
   );
