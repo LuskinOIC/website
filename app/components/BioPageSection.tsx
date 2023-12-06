851ce8d410c607abcce5467ca23a7881f6c6abb2
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-06 14:25:07 -0800

    Adjusted padding bottom for bio page sections

--- app/components/BioPageSection.tsx
+++ app/components/BioPageSection.tsx
@@ -13,14 +13,14 @@ const ContentParagraph = ({ contentItem }: { contentItem: any }) => {
 const BioPageSection = ({ section }: { section: BioPageSectionType }) => (
   <div key={section.sys.id}>
     <h2 className="text-lg font-bold lg:mb-4">{section.fields.title}</h2>
-    <div className="text-base lg:pb-6">
+    <div className="text-base lg:pb-10">
       {section.fields.content.content.map(
         (
           contentItem: { content: Array<{ value: string }> },
-          contentIndex: number,
+          contentIndex: number
         ) => (
           <ContentParagraph key={contentIndex} contentItem={contentItem} />
-        ),
+        )
       )}
     </div>
   </div>
