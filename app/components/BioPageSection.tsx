9e84783b53ecf2b0a20842f8c1819a400b214993
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-06 14:25:07 -0800

    Adjusted padding bottom for bio page sections

--- app/components/BioPageSection.tsx
+++ app/components/BioPageSection.tsx
@@ -10,42 +10,20 @@ const ContentParagraph = ({ contentItem }: { contentItem: any }) => {
   );
 };
 
-// const BioPageSection = ({ section, index }: { section: BioPageSectionType, index: number }) => (
-
-//   <div key={section.sys.id}>
-//     <h2 className="text-lg font-bold lg:mb-4">{section.fields.title}</h2>
-//     <div className="text-base lg:pb-10">
-//       {section.fields.content.content.map(
-//         (
-//           contentItem: { content: Array<{ value: string }> },
-//           contentIndex: number
-//         ) => (
-//           <ContentParagraph key={contentIndex} contentItem={contentItem} />
-//         )
-//       )}
-//     </div>
-//   </div>
-// );
-
-// export default BioPageSection;
-
-interface SectionProps {
-  section: BioPageSectionType;
-}
-export default function BioPageSection({ section }: SectionProps) {
-  return (
-    <div key={section.sys.id}>
-      <h2 className="text-lg font-bold lg:mb-4">{section.fields.title}</h2>
-      <div className="text-base lg:pb-10">
-        {section.fields.content.content.map(
-          (
-            contentItem: { content: Array<{ value: string }> },
-            contentIndex: number
-          ) => (
-            <ContentParagraph key={contentIndex} contentItem={contentItem} />
-          )
-        )}
-      </div>
+const BioPageSection = ({ section }: { section: BioPageSectionType }) => (
+  <div key={section.sys.id}>
+    <h2 className="text-lg font-bold lg:mb-4">{section.fields.title}</h2>
+    <div className="text-base lg:pb-10">
+      {section.fields.content.content.map(
+        (
+          contentItem: { content: Array<{ value: string }> },
+          contentIndex: number
+        ) => (
+          <ContentParagraph key={contentIndex} contentItem={contentItem} />
+        )
+      )}
     </div>
-  );
-}
+  </div>
+);
+
+export default BioPageSection;
