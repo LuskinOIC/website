7aad434997e84e183a980c08877394b097412c89
Spencer Ward <swkineo@gmail.com>
2023-12-07 16:28:12 -0500

    Split social media banner into subcomponents

--- app/components/SocialMediaBanner.tsx
+++ app/components/SocialMediaBanner.tsx
@@ -3,50 +3,83 @@ import FB from "../../public/fb.svg";
 import Image from "next/image";
 import LinkedIN from "../../public/libanner.svg";
 import Ozzie from "../../public/Ozzie Crutches SVG 2.png";
+import OzzieMobile from "../../public/Ozzie Peeking In.png";
 import TwitterX from "../../public/TwitterX.svg";
 import Yelp from "../../public/yelpbanner.svg";
 import Youtube from "../../public/youtubebanner.svg";
+import { Text1 } from "./ui/Typography/Text";
+import { Title1 } from "./ui/Typography/Title";
 
 export default function SocialMediaBanner() {
   return (
     <div className="bg-[#FCE385]">
-      <div className="grid grid-rows-2 md:gap-x-6 md:grid-flow-col py-4 px-3 justify-center">
-        <div className="row-span-3 ...">
-          <div className="flex flex-row min-h-[75%] justify-center items-center">
-            <Image src={Ozzie} alt="Ozzie" />
-          </div>
+      <div className="grid grid-rows-2 md:gap-x-6 md:grid-flow-col py-4 justify-center">
+        <DesktopBanner />
+        <MobileBanner />
+        <SocialMediaIcons />
+      </div>
+    </div>
+  );
+}
+
+function DesktopBanner() {
+  return (
+    <div className="hidden md:block px-3">
+      <div className="row-span-3 ...">
+        <div className="flex flex-row min-h-[75%] justify-center items-center">
+          <Image src={Ozzie} alt="Ozzie" />
         </div>
-        <div className="col-span-1 ..">
-          <div className="text-4xl flex flex-row min-h-[50%] justify-center items-center text-center">
-            <h1>Follow our story on social media!</h1>
-          </div>
-          <div className="text-md text-center">
-            <p>
-              Join us in our mission to transform musculoskeletal care for all,
-              one child at a time,
-            </p>{" "}
-            <p>through education, research, and unwavering support.</p>
-          </div>
+      </div>
+      <div className="hidden md:block col-span-1 ..">
+        <div className="text-4xl flex flex-row min-h-[50%] justify-center items-center text-center">
+          <h1>Follow our story on social media!</h1>
         </div>
-        <div className="row-span-1  .. py-4 md:py-0">
-          <div className="flex flex-row min-h-[75%] justify-center items-center space-x-4 ..">
-            <a href="https://www.instagram.com/orthokids_losangeles/ ..">
-              <Image src={FB} alt="Facebook Logo" />
-            </a>
-            <a href="https://www.linkedin.com/school/luskin-orthopaedic-institute-for-children/">
-              <Image src={LinkedIN} alt="LinkedIn Logo" />
-            </a>
-            <a href="https://twitter.com/i/flow/login?redirect_after_login=%3FLuskinOIC">
-              <Image src={TwitterX} alt="X Logo" />
-            </a>
-            <a href="https://www.youtube.com/channel/UC3JqUFKs4TmQUGa9ekOYePA">
-              <Image src={Youtube} alt="Youtube Logo" />
-            </a>
-            <a href="https://www.yelp.com/biz/luskin-orthopaedic-institute-for-children-los-angeles">
-              <Image src={Yelp} alt="Yelp Logo " />
-            </a>
-          </div>
+        <div className="text-md text-center">
+          <p>
+            Join us in our mission to transform musculoskeletal care for all,
+            one child at a time,
+          </p>{" "}
+          <p>through education, research, and unwavering support.</p>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function MobileBanner() {
+  return (
+    <div className="block md:hidden">
+      <div className="flex flex-row items-center">
+        <Title1 className="pl-8">Follow our story on social media!</Title1>
+        <Image src={OzzieMobile} alt="Ozzie" />
+      </div>
+      <Text1 className="px-8">
+        Join us in our mission to transform musculoskeletal care for all, one
+        child at a time, through education, research, and unwavering support.
+      </Text1>
+    </div>
+  );
+}
+
+function SocialMediaIcons() {
+  return (
+    <div className="row-span-1  .. py-8 md:py-0 px-3">
+      <div className="flex flex-row min-h-[75%] justify-center items-center space-x-4 ..">
+        <a href="https://www.instagram.com/orthokids_losangeles/ ..">
+          <Image src={FB} alt="Facebook Logo" />
+        </a>
+        <a href="https://www.linkedin.com/school/luskin-orthopaedic-institute-for-children/">
+          <Image src={LinkedIN} alt="LinkedIn Logo" />
+        </a>
+        <a href="https://twitter.com/i/flow/login?redirect_after_login=%3FLuskinOIC">
+          <Image src={TwitterX} alt="X Logo" />
+        </a>
+        <a href="https://www.youtube.com/channel/UC3JqUFKs4TmQUGa9ekOYePA">
+          <Image src={Youtube} alt="Youtube Logo" />
+        </a>
+        <a href="https://www.yelp.com/biz/luskin-orthopaedic-institute-for-children-los-angeles">
+          <Image src={Yelp} alt="Yelp Logo " />
+        </a>
       </div>
     </div>
   );
