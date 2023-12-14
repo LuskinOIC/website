f400de9fc0b0304d10eed940abd0aefac1d2e2cc
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-14 14:31:05 -0800

    Edits from npm run lint

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -77,7 +77,7 @@ export default async function Event({ params }: { params: { slug: string } }) {
           <h1 className="my-6 text-2xl font-bold">{orgEvent.eventName}</h1>
           {renderRichTextToReactComponent(
             orgEvent.eventSummary as unknown as Document,
-            eventSummaryClassNames
+            eventSummaryClassNames,
           )}
           <p>Event Date: {formattedDateTime}</p>
           <div className="flex flex-col">
@@ -109,7 +109,7 @@ export default async function Event({ params }: { params: { slug: string } }) {
                 cardContent={patientObject.fields}
                 styleProps={patientMinimalCardStyleProps}
               />
-            )
+            ),
           )}
       </div>
       <div id="event-details" className="flex flex-col-reverse">
