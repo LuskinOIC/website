2c0db9ceffc07ee70a6a85e1070501a5ab922bc0
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-15 16:28:00 -0800

    Removed test comment

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -17,7 +17,7 @@ export async function generateStaticParams() {
   const events = await getEvents();
   return events.map((evt) => ({ slug: evt.slug }));
 }
-// TESTING NPM RUN BUILD
+
 // DATE TIME FORMATTING
 function formatDateTime(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
