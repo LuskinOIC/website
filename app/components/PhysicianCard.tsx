018af347ce5e4043a5d031654f888b13d90fdad6
Spencer Ward <swkineo@gmail.com>
2023-12-18 17:08:49 -0500

    Add basic styling to Individual Specialty page

--- app/components/PhysicianCard.tsx
+++ app/components/PhysicianCard.tsx
@@ -9,15 +9,15 @@ export default function PhysicianCard({
   physician: PhysicianBioType;
 }) {
   return (
-    <Card>
-      <div className="flex flex-col">
+    <Card className="w-fit h-fit">
+      <div className="flex flex-col items-center h-min px-5 py-5">
         <Image
           src={physician.physicianPortrait.fields.file.url}
           width={219}
           height={273}
           alt={physician.physicianPortrait.fields.description}
         />
-        <Title3>{physician.physicianName}</Title3>
+        <Title3 className="leading-[30px]">{physician.physicianName}</Title3>
       </div>
     </Card>
   );
--- app/specialties/[slug]/page.tsx
+++ app/specialties/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import PhysicianCard from "@/app/components/PhysicianCard";
 import TabSection from "@/app/components/TabSection";
-import { Title1 } from "@/app/components/ui/Typography/Title";
+import { Title1, Title3 } from "@/app/components/ui/Typography/Title";
 import { getSpecialties, getSpecialtyBySlug } from "@/app/utils/contentful";
 import renderRichTextToReactComponent from "@/app/utils/rich-text";
 import Image from "next/image";
@@ -21,20 +21,24 @@ export default async function Specialty({
 
   return (
     <main>
-      <div className="flex flex-row">
-        <div className="flex flex-col">
-          <Title1>{specialty.name}</Title1>
-          {renderRichTextToReactComponent(specialty.description)}
-        </div>
+      <div className="flex flex-row-reverse w-full pl-[10%] mt-[2%] items-center">
         <Image
           src={specialty.image.fields.file.url}
           width={720}
           height={720}
           alt={specialty.name}
+          className="col-span-4 w-1/2"
         />
+        <div className="flex flex-col w-[41%] mr-[3%] text-xl">
+          <Title1 className="font-normal">{specialty.name}</Title1>
+          {renderRichTextToReactComponent(specialty.description)}
+        </div>
       </div>
       <TabSection fields={specialty.tabSection.fields} />
-      <div className="grid">
+      <Title3 className="pl-[15%] mt-[-40px] ml-4 leading-10 uppercase text-luskin-blue font-medium">
+        {specialty.specialistsTitle}
+      </Title3>
+      <div className="w-full px-[15%] grid mb-5">
         {specialty.physicians.map((phys, index) => (
           <PhysicianCard key={index} physician={phys.fields} />
         ))}
