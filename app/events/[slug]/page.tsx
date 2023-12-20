dcbd4df438272276b356539fb035614cef559228
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-19 16:27:28 -0800

    Adjusted styling for mobile slider, reduced sponsor image size

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -86,7 +86,7 @@ export default async function Event({ params }: { params: { slug: string } }) {
           src={asset.src}
           width={asset.width}
           height={asset.height}
-          className="mx-4 my-4 h-24 w-24 object-contain"
+          className="mx-4 my-4 h-20 w-20 object-contain"
         />
       ))}
     </div>
@@ -96,28 +96,25 @@ export default async function Event({ params }: { params: { slug: string } }) {
     <main>
       <div
         className="flex flex-col-reverse md:flex-row md:justify-between"
-        id="main-event"
-      >
+        id="main-event">
         <div className="mx-10 md:ml-36 md:mt-24 md:w-4/12" id="main-event-info">
           <h1 className="my-6 text-2xl font-bold md:mb-8 md:text-4xl md:font-normal">
             {orgEvent.eventName}
           </h1>
           {renderRichTextToReactComponent(
             orgEvent.eventSummary as unknown as Document,
-            eventSummaryClassNames,
+            eventSummaryClassNames
           )}
           <p className="mb-4 text-lg md:mb-6 md:text-2xl">
             Event Date: {formattedDateTime}
           </p>
           <div className="flex flex-col md:mb-32 md:flex-row">
             <button
-              className={`md:mr-6 mb-4 w-44 rounded-lg bg-[#0076AD] py-1 text-lg tracking-wide uppercase text-white ${buttonStyling}`}
-            >
+              className={`md:mr-6 mb-4 w-44 rounded-lg bg-[#0076AD] py-1 text-lg tracking-wide uppercase text-white ${buttonStyling}`}>
               attend event
             </button>
             <button
-              className={`text-left md:text-center mb-4 underline ${buttonStyling}`}
-            >
+              className={`text-left md:text-center mb-4 underline ${buttonStyling}`}>
               directions
             </button>
           </div>
@@ -143,13 +140,12 @@ export default async function Event({ params }: { params: { slug: string } }) {
                 key={patientObject.fields.title}
                 cardContent={patientObject.fields}
               />
-            ),
+            )
           )}
       </div>
       <div
         id="event-details"
-        className="flex flex-col-reverse md:mb-20 md:flex-row md:justify-between 2xl:ml-64"
-      >
+        className="flex flex-col-reverse md:mb-20 md:flex-row md:justify-between 2xl:ml-64">
         <p className="mx-6 mb-6 md:mt-48 md:w-2/5 md:text-2xl">
           {orgEvent.eventDetails}
         </p>
@@ -200,8 +196,7 @@ export default async function Event({ params }: { params: { slug: string } }) {
         </p>
         <div
           id="sponsor-assets"
-          className="my-6 flex flex-wrap border md:border-0"
-        >
+          className="my-6 flex flex-wrap border md:border-0">
           {hasSponsors && <Slider slides={sliderSlides as any} />}
         </div>
 
