3ac7d63659369fe8e28ef836508631118300a9f2
Spencer Ward <swkineo@gmail.com>
2023-12-19 19:49:43 -0500

    Add placeholder types and code

--- app/ways-to-give/page.tsx
+++ app/ways-to-give/page.tsx
@@ -1,5 +1,9 @@
+import { getWaysToGivePage } from "../utils/contentful";
+
 // This page represents the "ways to give" page.
-export default function WaysToGive() {
+export default async function WaysToGive() {
+  const page = await getWaysToGivePage();
+
   return (
     <main>
       <h1>Transform a Child&apos;s Future</h1>
