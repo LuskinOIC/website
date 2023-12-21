fc539f05851d1ffe503c9dc99457213f4356f2ce
Yoline <92892101+YolineB@users.noreply.github.com>
2023-12-21 12:32:39 -0800

    Temp fix: Patient care type declaration (#71)

--- app/patient-care/page.tsx
+++ app/patient-care/page.tsx
@@ -1,20 +1,21 @@
-import { getPageByType } from "@/app/utils/contentful";
-import { PAGE_TYPES } from "../constants/entries";
-import TabSection from "../components/TabSection";
-import LocationsCard from "../components/LocationsCard";
+// import { getPageByType } from "@/app/utils/contentful";
+// import { PAGE_TYPES } from "../constants/entries";
+// import TabSection from "../components/TabSection";
+// import LocationsCard from "../components/LocationsCard";
 // import InfoCardLayout from "../components/PageSection/InfoCardLayout";
 
 export default async function PatientCare() {
-  const patientCarePage = await getPageByType(PAGE_TYPES.PATIENT_CARE, 10);
+  // const patientCarePage = await getPageByType(PAGE_TYPES.PATIENT_CARE, 10);
 
   // TODO: Replace hardcoding with map and PageSection components
   return (
     <main>
-      <LocationsCard
+      <div>Under Construction</div>
+      {/* <LocationsCard
         locations={patientCarePage.pageSections[0].fields.locations}
-      />
+      /> */}
       {/* <InfoCardLayout section={patientCarePage.pageSections[0]} /> */}
-      <TabSection tabs={patientCarePage.pageSections[1].fields.tabs} />
+      {/* <TabSection tabs={patientCarePage.pageSections[1].fields.tabs} /> */}
     </main>
   );
 }
