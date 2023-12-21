9ccd6e0d14b4a6c49fb497bb1c752297cf278c7a
Spencer Ward <swkineo@gmail.com>
2023-12-21 16:42:04 -0500

    Fix TypeSript error (potentially undefined fields) (#72)

--- app/patient-care/page.tsx
+++ app/patient-care/page.tsx
@@ -1,21 +1,20 @@
-// import { getPageByType } from "@/app/utils/contentful";
-// import { PAGE_TYPES } from "../constants/entries";
-// import TabSection from "../components/TabSection";
-// import LocationsCard from "../components/LocationsCard";
+import { getPageByType } from "@/app/utils/contentful";
+import { PAGE_TYPES } from "../constants/entries";
+import TabSection from "../components/TabSection";
+import LocationsCard from "../components/LocationsCard";
+// TODO: Test or remove
 // import InfoCardLayout from "../components/PageSection/InfoCardLayout";
 
 export default async function PatientCare() {
-  // const patientCarePage = await getPageByType(PAGE_TYPES.PATIENT_CARE, 10);
+  const patientCarePage = await getPageByType(PAGE_TYPES.PATIENT_CARE, 10);
+  const locations = patientCarePage.pageSections[0].fields.locations;
+  const tabs = patientCarePage.pageSections[1].fields.tabs;
 
-  // TODO: Replace hardcoding with map and PageSection components
   return (
     <main>
-      <div>Under Construction</div>
-      {/* <LocationsCard
-        locations={patientCarePage.pageSections[0].fields.locations}
-      /> */}
+      {locations && <LocationsCard locations={locations} />}
       {/* <InfoCardLayout section={patientCarePage.pageSections[0]} /> */}
-      {/* <TabSection tabs={patientCarePage.pageSections[1].fields.tabs} /> */}
+      {tabs && <TabSection tabs={tabs} />}
     </main>
   );
 }
