e08fab09c9ae341e502199c9daf6bdd7ff591309
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-21 19:17:59 -0800

    Adjusted styling for Minimal Card component to reflect Figma design for event blog

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,7 +3,7 @@ import { MinimalCardType, EventCardType } from "@/app/constants/types";
 import MinimalCard from "@/app/components/MinimalCard";
 
 export function adaptEventToMinimalCardType(
-  event: EventCardType,
+  event: EventCardType
 ): MinimalCardType {
   return {
     title: event.eventName,
@@ -17,14 +17,25 @@ export default async function Events() {
 
   return (
     <main className="">
-      <h1 className="text-xl">Events</h1>
-      <div className="grid grid-cols-1 place-items-center">
-        {events.map((eventObj) => (
-          <MinimalCard
-            key={eventObj.slug}
-            cardContent={adaptEventToMinimalCardType(eventObj)}
-          />
-        ))}
+      {/* MOBILE Search Box */}
+      <div className="mx-auto flex flex-col md:ml-16 md:justify-center">
+        <p className="mb-4 mt-6 hidden text-lg text-[#0076AD] md:ml-4 md:block">
+          Back to Browse
+        </p>
+        <div className="md:hidden">
+          <h1>add search bar</h1>
+        </div>
+        <h1 className="mb-2 ml-12 mt-8 text-xl font-bold uppercase text-[#0076AD] md:mb-4 md:ml-4 md:text-3xl md:font-normal md:capitalize">
+          Events
+        </h1>
+        <div className="flex flex-col flex-wrap md:flex-row">
+          {events.map((eventObj) => (
+            <MinimalCard
+              key={eventObj.slug}
+              cardContent={adaptEventToMinimalCardType(eventObj)}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
