b896b652b13e1a32b239f105a773e96c99d498e3
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-21 17:13:40 -0800

    Initial commit for carousel, included arrows in Slider component

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { useSwipeable } from "react-swipeable";
 
-// Replace with actual used variables
 interface Slide {
   title: string;
   buttonText: string;
@@ -10,45 +9,9 @@ interface Slide {
 
 interface SliderProps {
   slides: Slide[];
-  navigationComponent?: React.ReactNode;
 }
 
-const Slider: React.FC<SliderProps> = ({
-  slides,
-  navigationComponent = null,
-}) => {
-  const LeftArrow = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="h-6 w-6">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.75 19.5L8.25 12l7.2-4.5"
-      />
-    </svg>
-  );
-
-  const RightArrow = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="h-6 w-6">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M8.25 4.5l7.5 7.5-7.5 7.5"
-      />
-    </svg>
-  );
-
+const Slider: React.FC<SliderProps> = ({ slides }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [transitionEnabled, setTransitionEnabled] = useState(true);
 
@@ -109,11 +72,6 @@ const Slider: React.FC<SliderProps> = ({
         ))}
       </div>
       <div className="absolute bottom-0 left-0 right-0 mb-2 flex items-center justify-center">
-        {navigationComponent && (
-          <button onClick={prevSlide}>
-            <LeftArrow />
-          </button>
-        )}
         {slides.map((_, index) => (
           <span
             key={index}
