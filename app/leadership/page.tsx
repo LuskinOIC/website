6422267853e306c65bf6af91fb78ff2589b729ac
Yoline <92892101+YolineB@users.noreply.github.com>
2023-12-22 14:50:01 -0800

    About, Leadership, Medical Professional data model update

--- app/leadership/page.tsx
+++ app/leadership/page.tsx
@@ -1,7 +1,15 @@
-export default function Leadership() {
+import PageSection from "@/app/components/PageSection/PageSection";
+import { PageSectionType } from "@/app/constants/types";
+import { getPageByType } from "@/app/utils/contentful";
+import { PAGE_TYPES } from "@/app/constants/entries";
+
+export default async function Leadership() {
+  const page = await getPageByType(PAGE_TYPES.LEADERSHIP);
   return (
     <main>
-      <h1>LuskinOIC&apos;s Leadership Drives Our Mission Forward</h1>
+      {page.pageSections.map((section: PageSectionType) => (
+        <PageSection key={section.fields.title} section={section} />
+      ))}
     </main>
   );
 }
