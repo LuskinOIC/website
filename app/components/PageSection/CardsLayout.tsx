3e2ca900d4e894b0da00a6a79b867f71110cc74b
Yoline <92892101+YolineB@users.noreply.github.com>
2023-12-25 22:27:39 -0800

    Render quad text component
    Does not yet receive contentful data

--- app/components/PageSection/CardsLayout.tsx
+++ app/components/PageSection/CardsLayout.tsx
@@ -4,6 +4,7 @@ import QuadCard from "../QuadComponent";
 
 //Components
 import InfoCardLayout from "./InfoCardLayout";
+import LegacyCard from "../LegacyCard";
 
 export default function ColumnLayout({
   section,
@@ -15,10 +16,16 @@ export default function ColumnLayout({
     cards && cards.filter((card) => card.fields.cardType === "Info Card");
   const quadCards =
     cards && cards.filter((card) => card.fields.cardType === "Quad Image Card");
+  const quadTextCards =
+    cards && cards.filter((card) => card.fields.cardType === "Quad Text Card");
+
   if (infoCards && infoCards.length > 0) {
     return <InfoCardLayout section={infoCards} />;
   }
   if (quadCards && quadCards.length > 0) {
     return <QuadCard section={quadCards} />;
+  }
+  if (quadTextCards && quadTextCards.length > 0) {
+    return <LegacyCard />;
   }
 }
