1cae06f8d97885018b01e37ce69addc2a02ab3de
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-29 17:58:55 -0800

    TriImage layout now expands across width of page

--- app/components/PageSection/TriImageLayout.tsx
+++ app/components/PageSection/TriImageLayout.tsx
@@ -9,15 +9,18 @@ type TriImageLayoutProps = {
 
 export default function TriImageLayout({ section }: TriImageLayoutProps) {
   return (
-    <section className="flex">
+    <section className="flex w-full">
       {section.map((image, i: number) => {
         return (
-          <div key={i} className="">
+          <div key={i} className="w-full">
             <Image
               src={image.fields.file.url}
               alt={image.fields.file.fileName}
-              width={500}
-              height={500}
+              // width={500}
+              width={image.fields.file.details.image.width}
+              // height={500}
+              height={image.fields.file.details.image.height}
+              className="w-full object-cover"
             />
           </div>
         );
