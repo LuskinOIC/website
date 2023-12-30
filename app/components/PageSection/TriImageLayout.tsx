71d169228bb5be8a1bc7ca8be24a4dae9baa2026
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-12-29 17:43:47 -0800

    Removed redundancy in defining image type in map function

--- app/components/PageSection/TriImageLayout.tsx
+++ app/components/PageSection/TriImageLayout.tsx
@@ -3,16 +3,26 @@ import Image from "next/image";
 // Types
 import { TriImageType } from "@/app/constants/types";
 
+type TriImageLayoutProps = {
+  section: TriImageType["fields"]["images"];
+};
+
 export default function TriImageLayout({
-  section,
-}: {
-  section: TriImageType[];
-}) {
+  section
+}: TriImageLayoutProps
+//   {
+//   section,
+// }: {
+//   TriImageType, getting errors
+//   section: object[];
+//   section: TriImageType[];
+// }
+) {
   return(
     <section className="flex">
       {/* map over images array from TriImageType */}
       {/* display image */}
-      {section.map((image: any, i: number) => {
+      {/* {section.map((image: any, i: number) => {
         return(
           <div
             key={i}
@@ -23,6 +33,18 @@ export default function TriImageLayout({
               // style={}
               width={500}
               height={500}
+            />
+          </div>
+        )
+      })} */}
+      {section.map((image, i:number) => {
+        return (
+          <div key={i} className="">
+            <Image
+            src={image.fields.file.url}
+            alt={image.fields.file.fileName}
+            width={500}
+            height={500}
             />
           </div>
         )
