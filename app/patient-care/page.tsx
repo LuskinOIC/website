8119d8857163717472a6562195046d0e9119f517
Yoline <92892101+YolineB@users.noreply.github.com>
2024-01-02 17:19:37 -0800

    Correct Patient Care types call

--- app/patient-care/page.tsx
+++ app/patient-care/page.tsx
@@ -2,19 +2,21 @@ import { getPageByType } from "@/app/utils/contentful";
 import { PAGE_TYPES } from "../constants/entries";
 import TabSection from "../components/TabSection";
 import LocationsCard from "../components/LocationsCard";
+import { PageSectionType } from "../constants/types";
 // TODO: Test or remove
 // import InfoCardLayout from "../components/PageSection/InfoCardLayout";
 
 export default async function PatientCare() {
   const patientCarePage = await getPageByType(PAGE_TYPES.PATIENT_CARE, 10);
-  const locations = patientCarePage.pageSections[0].fields.locations;
-  const tabs = patientCarePage.pageSections[1].fields.tabs;
+  const pageSections =
+    patientCarePage.pageSections as unknown as PageSectionType[];
+  const locations = pageSections[0]?.fields?.locations;
+  const tabs = pageSections[1]?.fields?.tabs;
 
   // TODO: Replace hardcoding with map and PageSection components
   return (
     <main>
       {locations && <LocationsCard locations={locations} />}
-      {/* <InfoCardLayout section={patientCarePage.pageSections[0]} /> */}
       {tabs && <TabSection tabs={tabs} />}
     </main>
   );
