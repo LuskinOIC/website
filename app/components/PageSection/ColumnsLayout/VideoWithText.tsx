3b54be5a18d7aaa691b74bc32d7c8b412712512b
Yoline <92892101+YolineB@users.noreply.github.com>
2024-01-03 23:09:26 -0800

    Correct fullScreen call

--- app/components/PageSection/ColumnsLayout/VideoWithText.tsx
+++ app/components/PageSection/ColumnsLayout/VideoWithText.tsx
@@ -17,7 +17,7 @@ export default function VideoWithText({ section }: { section: ColumnType }) {
             <div className="aspect-video">
               <iframe
                 src={section.fields.video}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowFullscreen"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 className="w-full h-full"
               />
