743ebb688537bbed6e66a5319291c4c9101f647d
Spencer Ward <swkineo@gmail.com>
2024-01-07 12:08:00 -0500

    Clean up and style Quad Text Cards for desktop and mobile

--- app/components/QuadTextCard.tsx
+++ app/components/QuadTextCard.tsx
@@ -5,63 +5,39 @@ import email from "@/public/email.svg";
 import { CardType } from "../constants/types";
 import renderRichTextToReactComponent from "../utils/rich-text";
 
-// const cardContents = [
-//   {
-//     title: "Sponsor an Event",
-//     description:
-//       "Sponsoring or attending a LuskinOIC event is a fun way to support children and promote your company or organization.",
-//     contact: "To find out more contact: Mary Beth Perrine",
-//   },
-//   {
-//     title: "Donation of Stocks",
-//     description:
-//       "Stocks and securities are a great way to further LuskinOIC's mission.",
-//     contact: "To find out more contact: Mary Beth Perrine",
-//   },
-//   {
-//     title: "Naming Opportunities",
-//     description:
-//       "Naming opportunities create lasting connections to places you cherish. Whether it's exam rooms, equipment, or more, we'll find the perfect fit for you.",
-//     contact: "To find out more contact: Mary Beth Perrine",
-//   },
-//   {
-//     title: "Planned Legacy Gifts",
-//     description:
-//       "Leaving a legacy is easy. Include LuskinOIC in your will, set up a charitable trust, or donate property, and you'll be promptly recognized in our legacy society.",
-//     contact: "To find out more contact: Mary Beth Perrine",
-//   },
-// ];
-
 export default function QuadTextCard({ section }: { section: CardType[] }) {
-  console.dir(`section: ${section}`);
   return (
     <section className="flex justify-center">
-      <div className="grid md:w-[80%] md:grid-cols-2 gap-6 justify-items-center p-5 md:p-10">
+      <div className="grid w-[80%] md:grid-cols-2 gap-6 justify-items-center py-5 md:py-10">
         {section.map((card, index) => {
           const cardContent = card.fields.cardContent;
           const cardLocation = cardContent ? cardContent.fields.location : null;
           return (
-            <div key={index} className="w-full bg-[#FFF2C0] rounded p-4">
-              <h1>
+            <div
+              key={index}
+              className="flex flex-col flex-grow w-full h-full bg-[#FFF2C0] rounded p-7"
+            >
+              <h1 className="mb-2">
                 <b>{card.fields.title}</b>
               </h1>
               {cardContent &&
                 cardContent.fields.content &&
                 renderRichTextToReactComponent(cardContent.fields.content)}
-              {cardLocation && (
-                <div>
-                  {cardLocation.fields.phoneNumber && (
-                    <div className="flex space-x-4">
-                      <Image src={phone} alt="phone" height={20} width={20} />
-                      <span>{cardLocation.fields.phoneNumber}</span>
-                    </div>
-                  )}
-                  {cardLocation.fields.emailAddress && (
-                    <div className="flex space-x-4">
-                      <Image src={email} alt="email" height={10} width={20} />
-                      <span>{cardLocation.fields.emailAddress}</span>
-                    </div>
-                  )}
+              <div className="flex grow" />
+              {cardLocation && cardLocation.fields.phoneNumber && (
+                <div className="flex space-x-4 my-2">
+                  <Image src={phone} alt="phone" height={20} width={20} />
+                  <span>{cardLocation.fields.phoneNumber}</span>
+                </div>
+              )}
+              {cardLocation && cardLocation.fields.emailAddress && (
+                <div className="justify-self-end flex space-x-4">
+                  <Image src={email} alt="email" height={10} width={20} />
+                  <span>
+                    <a href={`mailto:${cardLocation.fields.emailAddress}`}>
+                      {cardLocation.fields.emailAddress}
+                    </a>
+                  </span>
                 </div>
               )}
             </div>
