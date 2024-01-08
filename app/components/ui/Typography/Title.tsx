a6700cefa13b80c21f9cedd5b197307c6768f309
Yoline <92892101+YolineB@users.noreply.github.com>
2024-01-07 22:27:33 -0800

    Correct header line height and font

--- app/components/ui/Typography/Title.tsx
+++ app/components/ui/Typography/Title.tsx
@@ -31,13 +31,16 @@ export const Title0 = ({ children, className = "" }: TitleProps) => {
 };
 
 export const Title1 = ({ children, className = "" }: TitleProps) => {
-  const classes = cn("text-2xl md:text-3xl md:leading-[30px] py-2", className);
+  const classes = cn(
+    "text-[28px] md:text-[32px] leading-10 py-2 font-bold",
+    className,
+  );
   return <h1 className={classes}>{children}</h1>;
 };
 
 export const Title2 = ({ children, className = "" }: TitleProps) => {
   const classes = cn(
-    "text-xl md:text-2xl font-arial md:leading-[60px] py-2",
+    "text-xl md:text-2xl font-arial leading-7 py-2",
     className,
   );
   return <h1 className={classes}>{children}</h1>;
