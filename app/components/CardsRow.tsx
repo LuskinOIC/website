f2bc11e009a4630c963197326a0baa7f4a6dd6c5
Spencer Ward <swkineo@gmail.com>
2024-01-09 16:51:25 -0500

    Add links to Leadership cards

--- app/components/CardsRow.tsx
+++ app/components/CardsRow.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import { CardsRowPropsType, CardsRowType } from "../constants/types";
 import MinimalCard from "./MinimalCard";
 
 const CardsRow = ({ title, cards }: CardsRowPropsType) => {
   const hasCards: boolean = Array.isArray(cards) && cards.length > 0;
+  console.dir(cards);
   return (
     <section className="block px-5 md:px-32">
       <h1 className="mb-2 mt-8 text-xl font-bold uppercase text-[#0076AD] md:mb-4 md:ml-4 md:text-3xl md:font-normal md:capitalize">
@@ -10,15 +12,25 @@ const CardsRow = ({ title, cards }: CardsRowPropsType) => {
       </h1>
       <div className="flex flex-wrap md:flex-row-4 md:gap-10">
         {hasCards &&
-          cards.map((card: CardsRowType) => (
-            <MinimalCard
-              key={card.sys.id}
-              cardContent={{
-                title: card.fields.name,
-                cardPhoto: card.fields.portrait,
-              }}
-            />
-          ))}
+          cards.map((card: CardsRowType) => {
+            let href = "";
+            if ("affiliations" in card.fields) {
+              href = `/physicians/${card.fields.slug}`;
+            } else {
+              href = `/members/${card.fields.slug}`;
+            }
+            return (
+              <Link key={`linkto:${card.sys.id}`} href={href} className="">
+                <MinimalCard
+                  key={card.sys.id}
+                  cardContent={{
+                    title: card.fields.name,
+                    cardPhoto: card.fields.portrait,
+                  }}
+                />
+              </Link>
+            );
+          })}
       </div>
     </section>
   );
