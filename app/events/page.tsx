84ffeba00ddfe89d69b21690bb2d45489be06d7e
Yoline <92892101+YolineB@users.noreply.github.com>
2024-01-09 12:48:45 -0800

    add BackToBrowse and SearchBar

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,24 +1,16 @@
 import { getEvents } from "@/app/utils/contentful";
 import { EventCardType } from "@/app/constants/types";
 import MinimalCard from "@/app/components/MinimalCard";
+import BackToBrowse from "../components/ui/BackToBrowse";
+import SearchBar from "../components/ui/SearchBar";
 
 export default async function Events() {
   const events = (await getEvents()) as unknown as EventCardType[];
   return (
     <main className="">
-      {/* MOBILE Search Box */}
       <div className="mx-auto flex flex-col md:ml-16 md:justify-center">
-        {/* Todo: Add back arrow */}
-        <p className="mb-4 mt-6 hidden text-lg text-[#0076AD] md:ml-4 md:block">
-          Back to Browse
-        </p>
-        {/* Todo: add search box */}
-        <div className="md:hidden">
-          <h1>insert search box</h1>
-        </div>
-        <h1 className="mb-2 ml-12 mt-8 text-xl font-bold uppercase text-[#0076AD] md:mb-4 md:ml-4 md:text-3xl md:font-normal md:capitalize">
-          Events
-        </h1>
+        <BackToBrowse />
+        <SearchBar />
         <div className="flex flex-col flex-wrap md:flex-row">
           {events.map((eventObj) => (
             <MinimalCard
