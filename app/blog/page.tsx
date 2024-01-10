4264262c7b3750ec927a3bd6906d7fe5fc34b3e7
Yoline <92892101+YolineB@users.noreply.github.com>
2024-01-10 13:42:15 -0800

    [LUS-162] Card Rows Events (#102)
    * add BackToBrowse and SearchBar
    
    * Add blogCardsRow to events/page, update Contentful events
    
    * Add events row in events/[slug]
    
    * Add events section on blog page
    
    * Add 'see all' on mobile

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,18 +1,21 @@
 import PageSection from "@/app/components/PageSection/PageSection";
 import { PageSectionType, BlogCardsRowType } from "@/app/constants/types";
-import { getPageByType, getNewsPosts } from "@/app/utils/contentful";
+import { getPageByType, getNewsPosts, getEvents } from "@/app/utils/contentful";
 import { PAGE_TYPES } from "@/app/constants/entries";
 import BlogCardsRow from "../components/BlogCardsRow";
 
 export default async function Blog() {
   const page = await getPageByType(PAGE_TYPES.BLOG);
   const news = (await getNewsPosts(8)) as unknown as BlogCardsRowType[];
+  const events = (await getEvents(8)) as unknown as BlogCardsRowType[];
+
   return (
     <main>
       {page.pageSections.map((pageSection: PageSectionType) => (
         <PageSection key={pageSection.fields.title} section={pageSection} />
       ))}
-      <div className="grid mx-auto w-4/5">
+      <div className="mx-5 md:mx-[5%]">
+        <BlogCardsRow type="events" cards={events} />
         <BlogCardsRow type="news" cards={news} />
       </div>
     </main>
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,35 +1,18 @@
 import { getEvents } from "@/app/utils/contentful";
-import { EventCardType } from "@/app/constants/types";
-import MinimalCard from "@/app/components/MinimalCard";
+import { BlogCardsRowType } from "@/app/constants/types";
+import BackToBrowse from "../components/ui/BackToBrowse";
+import SearchBar from "../components/ui/SearchBar";
+import BlogCardsRow from "../components/BlogCardsRow";
 
 export default async function Events() {
-  const events = (await getEvents()) as unknown as EventCardType[];
+  const events = (await getEvents()) as unknown as BlogCardsRowType[];
   return (
     <main className="">
-      {/* MOBILE Search Box */}
       <div className="mx-auto flex flex-col md:ml-16 md:justify-center">
-        {/* Todo: Add back arrow */}
-        <p className="mb-4 mt-6 hidden text-lg text-[#0076AD] md:ml-4 md:block">
-          Back to Browse
-        </p>
-        {/* Todo: add search box */}
-        <div className="md:hidden">
-          <h1>insert search box</h1>
-        </div>
-        <h1 className="mb-2 ml-12 mt-8 text-xl font-bold uppercase text-[#0076AD] md:mb-4 md:ml-4 md:text-3xl md:font-normal md:capitalize">
-          Events
-        </h1>
+        <BackToBrowse />
+        <SearchBar />
         <div className="flex flex-col flex-wrap md:flex-row">
-          {events.map((eventObj) => (
-            <MinimalCard
-              key={eventObj.slug}
-              cardContent={{
-                title: eventObj.eventName,
-                cardPhoto: eventObj.eventPhoto,
-                summary: eventObj.eventSummary,
-              }}
-            />
-          ))}
+          <BlogCardsRow type="events" cards={events} />
         </div>
       </div>
     </main>
