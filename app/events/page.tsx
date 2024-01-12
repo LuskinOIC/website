bb80dba29ff04d7e991b4f303ce704b5dc26557e
Spencer Ward <swkineo@gmail.com>
2024-01-12 16:45:38 -0500

    Comment out SearchBar in Events page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,16 +1,17 @@
 import { getEvents } from "@/app/utils/contentful";
 import { BlogCardsRowType } from "@/app/constants/types";
 import BackToBrowse from "../components/ui/BackToBrowse";
-import SearchBar from "../components/ui/SearchBar";
+// import SearchBar from "../components/ui/SearchBar";
 import BlogCardsRow from "../components/BlogCardsRow";
 
+// TODO: Replace SearchBar with a dedicated SearchAreaEvents component
 export default async function Events() {
   const events = (await getEvents()) as unknown as BlogCardsRowType[];
   return (
     <main>
       <div className="flex flex-col px-[5%] md:justify-center">
         <BackToBrowse />
-        <SearchBar />
+        {/* <SearchBar /> */}
         <BlogCardsRow type="events" cards={events} />
       </div>
     </main>
