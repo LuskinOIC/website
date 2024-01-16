108acd11499feaadaaed1f17eb50300bc1db6246
iliadeleon <iliadeleon@gmail.com>
2024-01-16 12:09:31 -0500

    add hover state to card

--- app/components/MinimalCard.tsx
+++ app/components/MinimalCard.tsx
@@ -8,7 +8,7 @@ const titleImageComponentStyles = {
   image:
     "h-28 w-28 rounded-xl object-cover p-1.5 md:mx-auto md:h-5/6 md:w-10/12 md:rounded-sm md:px-4 md:pt-6",
   wrapperDiv:
-    "flex-reverse mb-3 flex h-28 w-full rounded-lg bg-[#0076AD] md:mb-8 md:h-full md:w-1/5 md:flex-col md:rounded-lg md:border md:bg-white",
+    "flex-reverse mb-3 flex h-28 w-full rounded-lg bg-[#0076AD] md:mb-8 md:h-full md:w-1/5 md:flex-col md:rounded-lg md:border md:bg-white hover:underline",
   header:
     "my-auto px-2 text-3xl font-semibold tracking-wider text-white md:pb-6 md:pt-4 md:text-center md:text-lg md:text-black",
   summary: "",
@@ -17,7 +17,7 @@ const titleImageComponentStyles = {
 const titleImageSummaryComponentStyles = {
   image: "rounded-xl w-80 md:w-[300px] h-[280px] object-cover",
   wrapperDiv:
-    "w-80 m-auto md:m-4 md:w-[300px] md:flex-shrink-0 flex flex-col mb-10",
+    "w-80 m-auto md:m-4 md:w-[300px] md:flex-shrink-0 flex flex-col mb-10 hover:underline",
   header: "md:text-center py-4 text-xl font-bold",
   summary: "mb-4 line-clamp-4 overflow-hidden leading-tight",
 };
@@ -38,7 +38,7 @@ export default function MinimalCard({
     typeof cardContent.summary === "string"
       ? cardContent.summary
       : renderRichTextToReactComponent(
-          cardContent.summary as unknown as Document,
+          cardContent.summary as unknown as Document
         );
 
   const selectedStyles = summary
