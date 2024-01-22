c5c35fa213db4a3a38f4e693c61a6d087dbebda2
Spencer Ward <swkineo@gmail.com>
2024-01-22 17:47:08 -0500

    Add useState and newsletter signup submitted state

--- app/components/NewsletterSignup.tsx
+++ app/components/NewsletterSignup.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import ozzie from "../../public/ozzie-wheelchair.png";
@@ -8,6 +10,8 @@ import { Text } from "./ui/Typography/Text";
 import { styles } from "./ui/Button";
 
 export default function NewsletterSignup() {
+  const [submitted, setSubmitted] = useState(false);
+
   const desktopButtonClass = cn(
     styles.buttonAlignment,
     styles.buttonContainer,
@@ -21,25 +25,43 @@ export default function NewsletterSignup() {
       <div className="flex items-center h-[36vw] w-[36vw] md:h-[13vw] md:w-[13vw] border-2 border-luskin-green rounded-full mb-[-18vw] md:mb-[-6.5vw] px-[0.9rem] bg-white z-10">
         <Image src={ozzie} alt="Ozzie" className="mx-auto" />
       </div>
-      <div className="flex flex-col justify-center items-center w-fit h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[20vw] md:pt-[7.5vw]">
-        <Title1 className="text-[24px] md:text-[32px] font-medium">
-          Join Our Newsletter!
-        </Title1>
-        <Text className="w-[70%] md:w-[75%] text-center mt-1">
-          To receive our latest news and events information join our newsletter
-          today.
-        </Text>
-        <div className="w-fit flex justify-center mt-8 mb-12 mx-3">
-          <input
-            type="text"
-            placeholder="Enter your email address"
-            className="w-fit text-left md:text-center border border-luskin-blue rounded-[5px] py-3 px-3 md:px-6 placeholder:opacity-50 placeholder:text-black placeholder:text-[0.92rem] md:placeholder:text-base"
-          />
-          <button className={desktopButtonClass}>
-            <span className="hidden md:block">Submit</span>
-            <Image src={arrow} alt="Submit" className="md:hidden mx-3" />
-          </button>
-        </div>
+      <div className="flex flex-col justify-center items-center md:w-[40vw] h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[20vw] md:pt-[7.5vw]">
+        {submitted ? (
+          <>
+            <Title1 className="text-[24px] md:text-[32px] text-center font-medium">
+              Thank You for
+              <br />
+              Unlocking the Magic!
+            </Title1>
+            <Text className="w-[70%] md:w-[75%] text-center mt-1 mb-16">
+              Stay tuned for LuskinOIC&apos;s latest news and events.
+            </Text>
+          </>
+        ) : (
+          <>
+            <Title1 className="text-[24px] md:text-[32px] font-medium">
+              Join Our Newsletter!
+            </Title1>
+            <Text className="w-[70%] md:w-[75%] text-center mt-1">
+              To receive our latest news and events information join our
+              newsletter today.
+            </Text>
+            <div className="w-fit flex justify-center mt-8 mb-12 mx-3">
+              <input
+                type="text"
+                placeholder="Enter your email address"
+                className="w-fit text-left md:text-center border border-luskin-blue rounded-[5px] py-3 px-3 md:px-6 placeholder:opacity-50 placeholder:text-black placeholder:text-[0.92rem] md:placeholder:text-base"
+              />
+              <button
+                className={desktopButtonClass}
+                onClick={() => setSubmitted(true)}
+              >
+                <span className="hidden md:block">Submit</span>
+                <Image src={arrow} alt="Submit" className="md:hidden mx-3" />
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
