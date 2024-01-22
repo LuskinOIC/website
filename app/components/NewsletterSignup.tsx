3ac8370afac309f84037ce696068c9990a5273e4
Spencer Ward <swkineo@gmail.com>
2024-01-22 12:08:56 -0500

    Adjust desktop sizing

--- app/components/NewsletterSignup.tsx
+++ app/components/NewsletterSignup.tsx
@@ -16,10 +16,10 @@ export default function NewsletterSignup() {
 
   return (
     <div className="w-full flex flex-col items-center m-14">
-      <div className="flex items-center h-[227px] w-[227px] border-2 border-luskin-green rounded-full mb-[-110px] bg-white z-10">
+      <div className="flex items-center h-[13vw] w-[13vw] border-2 border-luskin-green rounded-full mb-[-6.5vw] px-[0.9rem] bg-white z-10">
         <Image src={ozzie} alt="Ozzie" className="mx-auto" />
       </div>
-      <div className="flex flex-col justify-center items-center w-[45%] h-[40vh] border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[113px]">
+      <div className="flex flex-col justify-center items-center w-fit h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[7.5vw]">
         <Title1 className="">Join Our Newsletter!</Title1>
         <Text className="w-[60%] text-center">
           To receive our latest news and events information join our newsletter
@@ -29,7 +29,7 @@ export default function NewsletterSignup() {
           <input
             type="text"
             placeholder="Enter your email address"
-            className="w-[40%] text-center border border-luskin-blue rounded-[5px] p-3 placeholder:opacity-50 placeholder:text-black placeholder:text-lg"
+            className="w-fit text-center border border-luskin-blue rounded-[5px] py-3 px-6 placeholder:opacity-50 placeholder:text-black placeholder:text-base"
           />
           <button className={buttonClass}>Submit</button>
         </div>
