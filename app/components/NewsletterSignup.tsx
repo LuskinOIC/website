9703524542259c67437433b50d99e5d6f19f964e
Spencer Ward <swkineo@gmail.com>
2024-01-22 17:25:02 -0500

    Finish mobile styling (first pass)

--- app/components/NewsletterSignup.tsx
+++ app/components/NewsletterSignup.tsx
@@ -2,36 +2,47 @@ import React from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import ozzie from "../../public/ozzie-wheelchair.png";
+import arrow from "../../public/submit-arrow.png";
 import { Title1 } from "./ui/Typography/Title";
 import { Text } from "./ui/Typography/Text";
 import { styles } from "./ui/Button";
 
 export default function NewsletterSignup() {
-  const buttonClass = cn(
+  const desktopButtonClass = cn(
     styles.buttonAlignment,
     styles.buttonContainer,
     styles.buttonText,
-    "bg-luskin-blue ml-4",
+    "bg-luskin-blue ml-[0.6rem] md:ml-4",
+    // "hidden md:flex bg-luskin-blue ml-4",
+  );
+
+  const mobileButtonClass = cn(
+    styles.buttonAlignment,
+    styles.buttonContainer,
+    "md:hidden bg-luskin-blue ml-4", // flex already included in buttonAlignment
   );
 
   return (
-    <div className="w-full flex flex-col items-center my-14 mx-3 m:m-14">
-      <div className="flex items-center h-[36vw] w-[36vw] m:h-[13vw] m:w-[13vw] border-2 border-luskin-green rounded-full mb-[-18vw] m:mb-[-6.5vw] px-[0.9rem] bg-white z-10">
+    <div className="w-full flex flex-col items-center p-10 md:p-10">
+      <div className="flex items-center h-[36vw] w-[36vw] md:h-[13vw] md:w-[13vw] border-2 border-luskin-green rounded-full mb-[-18vw] md:mb-[-6.5vw] px-[0.9rem] bg-white z-10">
         <Image src={ozzie} alt="Ozzie" className="mx-auto" />
       </div>
-      <div className="flex flex-col justify-center items-center w-fit h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[20vw] m:pt-[7.5vw]">
-        <Title1 className="">Join Our Newsletter!</Title1>
+      <div className="flex flex-col justify-center items-center w-fit h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[20vw] md:pt-[7.5vw]">
+        <Title1 className="font-medium">Join Our Newsletter!</Title1>
         <Text className="w-[60%] text-center mt-1">
           To receive our latest news and events information join our newsletter
           today.
         </Text>
-        <div className="w-full flex justify-center mt-8 mb-12">
+        <div className="w-fit flex justify-center mt-8 mb-12 mx-3">
           <input
             type="text"
             placeholder="Enter your email address"
-            className="w-fit text-center border border-luskin-blue rounded-[5px] py-3 px-6 placeholder:opacity-50 placeholder:text-black placeholder:text-base"
+            className="w-fit text-left md:text-center border border-luskin-blue rounded-[5px] py-3 px-3 md:px-6 placeholder:opacity-50 placeholder:text-black placeholder:text-[0.9rem] md:placeholder:text-base"
           />
-          <button className={buttonClass}>Submit</button>
+          <button className={desktopButtonClass}>
+            <span className="hidden md:block">Submit</span>
+            <Image src={arrow} alt="Submit" className="md:hidden mx-3" />
+          </button>
         </div>
       </div>
     </div>
