946bee1962074eb8a9b9da40859ff2e1c57c3eb2
Spencer Ward <swkineo@gmail.com>
2024-01-23 13:07:58 -0500

    Make Ozzie smaller; split md: styling into md: and lg:

--- app/components/NewsletterSignup.tsx
+++ app/components/NewsletterSignup.tsx
@@ -21,10 +21,10 @@ export default function NewsletterSignup() {
 
   return (
     <div className="w-full flex flex-col items-center p-10 md:p-10">
-      <div className="flex items-center h-[36vw] w-[36vw] md:h-[13vw] md:w-[13vw] border-2 border-luskin-green rounded-full mb-[-18vw] md:mb-[-6.5vw] px-[0.9rem] bg-white z-10">
+      <div className="flex items-center h-[28vw] w-[28vw] md:h-[18vw] md:w-[18vw] lg:h-[10vw] lg:w-[10vw] border-2 border-luskin-green rounded-full mb-[-14vw] md:mb-[-9vw] lg:mb-[-5vw] px-[0.9rem] bg-white z-10">
         <Image src={ozzie} alt="Ozzie" className="mx-auto" />
       </div>
-      <div className="flex flex-col justify-center items-center md:w-[40vw] h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[20vw] md:pt-[7.5vw]">
+      <div className="flex flex-col justify-center items-center md:min-w-[40vw] h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[16vw] md:pt-[10vw] lg:pt-[5.5vw]">
         {submitted ? (
           <>
             <Title1 className="text-[24px] md:text-[32px] text-center font-medium">
@@ -41,7 +41,7 @@ export default function NewsletterSignup() {
             <Title1 className="text-[24px] md:text-[32px] font-medium">
               Join Our Newsletter!
             </Title1>
-            <Text className="w-[70%] md:w-[75%] text-center mt-1">
+            <Text className="w-[70%] md:w-[75%] lg:w-[55%] text-center mt-1">
               To receive our latest news and events information join our
               newsletter today.
             </Text>
@@ -49,7 +49,7 @@ export default function NewsletterSignup() {
               <input
                 type="text"
                 placeholder="Enter your email address"
-                className="w-fit text-left md:text-center border border-luskin-blue rounded-[5px] py-3 px-3 md:px-6 placeholder:opacity-50 placeholder:text-black placeholder:text-[0.92rem] md:placeholder:text-base"
+                className="w-fit text-left md:text-center border border-luskin-blue rounded-[5px] pt-2 pb-3 px-3 md:px-6 placeholder:opacity-50 placeholder:text-black placeholder:text-[0.92rem] md:placeholder:text-base"
               />
               <button
                 className={desktopButtonClass}
