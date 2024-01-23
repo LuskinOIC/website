8765a82d623ea3b790fffde33d737648863a3471
Crystal Shamsi <crystallynnvoss@gmail.com>
2024-01-23 10:55:48 -0700

    Appease linter

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,7 +12,9 @@ import {
 } from "@/components/ui/navigation-menu";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+// import { faMagnifyingGlass,} from "@fortawesome/free-solid-svg-icons";
+
 
 import Button from "@/app/components/ui/Button";
 import { DONATE_URL, MYCHART_URL, SAVE_MY_SPOT } from "../constants/links";
@@ -142,11 +144,9 @@ export default function Navbar() {
 
         {/* To be implemented later*/}
         <NavigationMenuItem className="list-none">
-          {/* <button className="bg-transparent text-white rounded-full p-3 text-xl"> */}
-            {" "}
-            {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
+          {/* <button className="bg-transparent text-white rounded-full p-3 text-xl"> */}{" "}
+          {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
           {/* </button> */}
-
           <button
             onClick={toggleHamburgerDropdown}
             className="bg-transparent text-white rounded-full py-3 px-4 text-xl"
