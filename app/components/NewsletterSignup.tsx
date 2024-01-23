490c29f0f0e3a370fefdc983905824d35c9eda60
Spencer Ward <swkineo@gmail.com>
2024-01-23 13:14:36 -0500

    Make mobile Ozzie larger

--- app/components/NewsletterSignup.tsx
+++ app/components/NewsletterSignup.tsx
@@ -21,10 +21,10 @@ export default function NewsletterSignup() {
 
   return (
     <div className="w-full flex flex-col items-center p-10 md:p-10">
-      <div className="flex items-center h-[28vw] w-[28vw] md:h-[18vw] md:w-[18vw] lg:h-[10vw] lg:w-[10vw] border-2 border-luskin-green rounded-full mb-[-14vw] md:mb-[-9vw] lg:mb-[-5vw] px-[0.9rem] bg-white z-10">
+      <div className="flex items-center h-[34vw] w-[34vw] md:h-[18vw] md:w-[18vw] lg:h-[10vw] lg:w-[10vw] border-2 border-luskin-green rounded-full mb-[-17vw] md:mb-[-9vw] lg:mb-[-5vw] px-[0.9rem] bg-white z-10">
         <Image src={ozzie} alt="Ozzie" className="mx-auto" />
       </div>
-      <div className="flex flex-col justify-center items-center md:min-w-[40vw] h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[16vw] md:pt-[10vw] lg:pt-[5.5vw]">
+      <div className="flex flex-col justify-center items-center md:min-w-[40vw] h-fit border border-luskin-green rounded-[20px] shadow-[0_4px_8px_2px_hsla(0,0%,0%,0.1)] z-0 pt-[18vw] md:pt-[10vw] lg:pt-[5.5vw]">
         {submitted ? (
           <>
             <Title1 className="text-[24px] md:text-[32px] text-center font-medium">
