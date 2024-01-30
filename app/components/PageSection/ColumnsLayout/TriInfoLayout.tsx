bd4ad48b87910cc33328b865a00c4fce776ee4ea
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-01-29 20:34:06 -0800

    Added styling and dividing lines

--- app/components/PageSection/ColumnsLayout/TriInfoLayout.tsx
+++ app/components/PageSection/ColumnsLayout/TriInfoLayout.tsx
@@ -4,7 +4,8 @@ import renderRichTextToReactComponent, {
 } from "@/app/utils/rich-text";
 import { CardType, TriCardContent } from "@/app/constants/types";
 
-const divider = "divide-x pl-10";
+const divider = "w-0.5 bg-[#825AA4]";
+const className = "text-center";
 export default function TriInfo({ section }: any) {
   const cardContents: TriCardContent[] = [];
   // console.log(section);
@@ -14,15 +15,22 @@ export default function TriInfo({ section }: any) {
   });
   console.log(cardContents.length - 1);
   return (
-    <div className="flex justify-center gap-32 bg-blue-400">
+    <div className="flex justify-center gap-20 py-10">
       {cardContents.map((cardContent: TriCardContent, i: number) => (
-        <div
-          key={i}
-          className={`${i > 0 && i < cardContents.length ? divider : ""}`}>
-          <h1>{cardContent.title}</h1>
-          <p>{}</p>
-          {cardContent.richContent &&
-            renderRichTextToReactComponent(cardContent.richContent)}
+        <div key={i} className="flex max-w-xs">
+          <div>
+            <h1
+              className={`my-2 text-lg ${
+                cardContent.bold ? "font-semibold" : ""
+              }`}>
+              {cardContent.title}
+            </h1>
+            <div className="pb-5">
+              {cardContent.richContent &&
+                renderRichTextToReactComponent(cardContent.richContent)}
+            </div>
+          </div>
+          <div className={`ml-16 ${i < 2 ? divider : ""}`}></div>
         </div>
       ))}
     </div>
