5b855f48489e83395fb4dee8bb1bbed7a89c6802
Lan Pham <lqpham21@yahoo.com>
2024-01-31 21:16:05 -0600

    Added mobile dropdown to blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
 import PageSection from "@/app/components/PageSection/PageSection";
-import { PageSectionType, BlogCardsRowType } from "@/app/constants/types";
+import { PageSectionType, BlogCardsRowType, OptionType} from "@/app/constants/types";
 import {
   getPageByType,
   getNewsPosts,
@@ -8,6 +8,7 @@ import {
 } from "@/app/utils/contentful";
 import { PAGE_TYPES } from "@/app/constants/entries";
 import BlogCardsRow from "../components/BlogCardsRow";
+import Dropdown from "../components/ui/Dropdown";
 
 export default async function Blog() {
   const page = await getPageByType(PAGE_TYPES.BLOG);
@@ -17,14 +18,24 @@ export default async function Blog() {
     8,
   )) as unknown as BlogCardsRowType[];
 
+  const dropdownOptions: OptionType[] = [
+    { label: "News", value: "news", targetID: "news" },
+    { label: "Events", value: "events", targetID: "events" },
+    { label: "Patient Stories", value: "patient-stories", targetID: "patient-stories" },
+  ];
+
   return (
     <main>
       {page.pageSections.map((pageSection: PageSectionType) => (
         <PageSection key={pageSection.fields.title} section={pageSection} />
       ))}
+      <div className="mb-12 md:hidden px-5">
+          <p className="px-1 pb-4">Choose a section you would like to review</p>
+          <Dropdown placeHolder="News" options={dropdownOptions} />
+        </div>
       <div className="mx-5 md:mx-[5%]">
-        <BlogCardsRow type="events" cards={events} />
         <BlogCardsRow type="news" cards={news} />
+        <BlogCardsRow type="events" cards={events} />
         <BlogCardsRow type="patient-stories" cards={patientStories} />
       </div>
     </main>
