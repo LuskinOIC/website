d0f46f487793a0a2fd4e93fa75c1344a70c5b403
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-05 09:05:27 -0800

    Refactor specialty phys card for responsive layout

--- app/components/PhysicianComponents/PhysicianCard.tsx
+++ app/components/PhysicianComponents/PhysicianCard.tsx
@@ -9,20 +9,21 @@ export default function PhysicianCard({
   physician: PhysicianBioType;
 }) {
   return (
-    <Link href={`/physicians/${physician.slug}`} className="hover:shadow-lg">
-      <div className="bg-[#0076AD] rounded-lg md:border md:bg-card md:text-card-foreground shadow-sm">
-        <div className="flex flex-row md:flex-col items-center h-28 md:h-auto">
-          <Image
-            src={physician.portrait.fields.file.url}
-            width={219}
-            height={273}
-            alt={physician.portrait.fields.description}
-            className="w-28 h-28 p-1.5 md:p-4 md:w-auto md:h-[273px]"
-          />
-          <Title3 className="leading-[30px] text-white md:text-black pl-3 md:px-4">
-            {physician.name}
-          </Title3>
-        </div>
+    <Link
+      href={`/physicians/${physician.slug}`}
+      className="md:min-h-[273px] bg-[#0076AD] rounded-lg md:border md:bg-card md:text-card-foreground shadow-sm hover:shadow-lg"
+    >
+      <div className="flex flex-row md:flex-col items-center ">
+        <Image
+          src={physician.portrait.fields.file.url}
+          width={219}
+          height={273}
+          alt={physician.portrait.fields.description}
+          className="w-28 h-28 p-1.5 md:w-auto md:h-auto"
+        />
+        <Title3 className="pl-3 md:px-2 text-center leading-[30px] text-white md:text-black">
+          {physician.name}
+        </Title3>
       </div>
     </Link>
   );
