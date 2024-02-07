0bf566e1603595669ef4236a3e273acf687066aa
Spencer Ward <swkineo@gmail.com>
2024-02-07 15:29:17 -0500

    Lower the physician search threshold slightly

--- app/components/PhysicianComponents/PhysicianSearchArea.tsx
+++ app/components/PhysicianComponents/PhysicianSearchArea.tsx
@@ -29,7 +29,7 @@ export default function PhysicianSearchArea({
     // findAllMatches: false,
     // minMatchCharLength: 1,
     // location: 0,
-    threshold: 0.35,
+    threshold: 0.32,
     // distance: 100,
     // useExtendedSearch: false,
     ignoreLocation: true,
