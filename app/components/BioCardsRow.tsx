248089be486566713fc2751dbf110e71353c3c14
Spencer Ward <swkineo@gmail.com>
2024-02-09 15:18:14 -0500

    Disable BioCardsRow links if a member does not have a page

--- app/components/BioCardsRow.tsx
+++ app/components/BioCardsRow.tsx
@@ -19,6 +19,12 @@ function getCardHref(card: CardsRowType) {
 const BioCardsRow = ({ title, cards }: BioCardsRowPropsType) => {
   const hasCards: boolean = Array.isArray(cards) && cards.length > 0;
 
+  cards.map((card: CardsRowType) => {
+    if (card.fields.name === "Maureen Stockton") {
+      console.dir(card.fields.topSection?.fields.description);
+    }
+  });
+
   return (
     <section className="block px-5 md:px-32">
       <Title3 className="mb-2 mt-8 font-bold uppercase text-[#0076AD] md:mb-4 md:ml-4 md:font-normal md:capitalize">
@@ -27,16 +33,36 @@ const BioCardsRow = ({ title, cards }: BioCardsRowPropsType) => {
       <div className="grid grid-rows md:grid-cols-4 gap-y-2 md:gap-x-2">
         {hasCards &&
           cards.map((card: CardsRowType, i: number) => {
-            let cardHref = getCardHref(card);
-            return (
-              <Link key={i} href={cardHref}>
+            // Add a link only if the member has a page
+            // (topSection for leadership, topSummary for physician)
+            if (
+              card.fields.topSection?.fields.subHeader ||
+              card.fields.topSection?.fields.description ||
+              card.fields.topSummary?.fields.subHeader ||
+              card.fields.topSummary?.fields.description
+            ) {
+              let cardHref = getCardHref(card);
+              return (
+                <Link key={`Leadership-BioCard-${i}`} href={cardHref}>
+                  <BioCard
+                    name={card.fields.name}
+                    portrait={card.fields.portrait}
+                    linked={true}
+                    classNames="h-full"
+                  />
+                </Link>
+              );
+            } else {
+              return (
                 <BioCard
+                  key={`Leadership-BioCard-${i}`}
                   name={card.fields.name}
                   portrait={card.fields.portrait}
+                  linked={false}
                   classNames="h-full"
                 />
-              </Link>
-            );
+              );
+            }
           })}
       </div>
     </section>
