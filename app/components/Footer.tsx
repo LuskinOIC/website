3871d7b37cff39290b856eda9205abaea97cfae4
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-12 15:53:23 -0800

    Correct naming of clinic + add hours (#173)

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -18,6 +18,19 @@ import {
 } from "../constants/links";
 import NewsletterSignup from "./NewsletterSignup";
 
+const socialMediaLinks = [
+  { url: FACEBOOK_URL, src: facebook, alt: "facebook" },
+  { url: LINKEDIN_URL, src: linkedin, alt: "linkedin" },
+  { url: TWITTER_URL, src: xlogo, alt: "twitter" },
+  { url: YOUTUBE_URL, src: youtube, alt: "youtube" },
+  { url: INSTAGRAM_URL, src: instagram, alt: "instagram" },
+  {
+    url: "https://www.yelp.com/biz/luskin-orthopaedic-institute-for-children-los-angeles",
+    src: yelp,
+    alt: "yelp",
+  },
+];
+
 export default function Footer() {
   // TODO: ensure appropriate divs/spans/etc elements placed appropriately for smoothest possible layout styling
   return (
@@ -26,7 +39,7 @@ export default function Footer() {
       {/* footer container */}
       <div
         id="container"
-        className="flex justify-around bg-[#0076AD] text-white pt-8 pb-11"
+        className="flex justify-around bg-[#0076AD] text-white py-8"
       >
         <div
           id="footer-left-content-container"
@@ -34,72 +47,72 @@ export default function Footer() {
         >
           <h1
             id="organization-header"
-            className="text-lg font-semibold tracking-wide text-left"
+            className="text-lg font-semibold tracking-wide text-left pb-2"
           >
             LuskinOIC Pediatric Orthopedic
           </h1>
           <div className="text-sm font-medium">
-            <div id="contact-info-container" className="pt-4 pb-6 flex ">
-              <div id="contact-info-hospital-uc" className="pr-10">
-                <h3 className="text-base pb-2.5">Hospital/Urgent Care</h3>
-                <div id="hospital-uc-address" className="pb-4">
-                  <p>403 West Adams Boulevard</p>
-                  <p>Los Angeles, CA 90007</p>
+            <div id="contact-info-container" className="flex">
+              <div className="flex flex-col">
+                <div id="contact-info-hospital-uc" className="pr-10 pb-2.5">
+                  <h3 className="text-base pb-2.5">
+                    Medical Pavilion & <br /> Ambulatory Surgery Center
+                  </h3>
+                  <div id="hospital-uc-address" className="pb-4">
+                    <p>403 West Adams Boulevard</p>
+                    <p>Los Angeles, CA 90007</p>
+                  </div>
+                  <p>(213) 742-1000</p>
                 </div>
-                <p>(213) 742-1000</p>
+                <div id="contact-info-hours" className="pr-10">
+                  <h3 className="text-base pb-2.5">Urgent Care Center Hours</h3>
+                  <div id="hours" className="pb-4">
+                    <p>Monday - Friday 8am - 4pm</p>
+                    <p>Closed Saturday & Sunday</p>
+                  </div>
+                </div>
               </div>
-              <div className="inline-block h-28 w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50 hidden lg:block"></div>
+
+              <div className="inline-block w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50 hidden lg:block"></div>
+
               <div id="contact-info-clinic" className="hidden lg:block pl-10">
-                <h3 className="text-base pb-2.5">Clinic</h3>
+                <h3 className="text-base pb-2.5">
+                  Luskin Children&apos;s Clinic
+                </h3>
                 <div id="clinic-address" className="pb-4">
                   <p>1250 16th Street, Suite 2100B</p>
                   <p>Santa Monica, CA 90404</p>
                 </div>
-                <p>(310) 395-4814</p>
+                <p className="pb-2.5">(310) 395-4814</p>
+                <div id="follow-container">
+                  <h3 className="text-sm pt-4">Follow LuskinOIC!</h3>
+                  <div id="social-media">
+                    <ul className="flex pt-3.5">
+                      {socialMediaLinks.map(({ url, src, alt }) => (
+                        <li key={alt} className="pr-3">
+                          <a href={url} target="_blank">
+                            <Image
+                              src={src}
+                              alt={alt}
+                              className=""
+                              width={32}
+                            />
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div id="follow-container">
-            <h3 className="text-sm pt-4">Follow LuskinOIC!</h3>
-            <div id="social-media">
-              <ul className="flex pt-3.5">
-                <li className="pr-3">
-                  <Link href={FACEBOOK_URL}>
-                    <Image src={facebook} alt="facebook" className="" />
-                  </Link>
-                </li>
-                <li className="pr-3">
-                  <Link href={LINKEDIN_URL}>
-                    <Image src={linkedin} alt="linkedin" className="" />
-                  </Link>
-                </li>
-                <li className="pr-3">
-                  <Link href={TWITTER_URL}>
-                    <Image src={xlogo} alt="twitter" className="" />
-                  </Link>
-                </li>
-                <li className="pr-3">
-                  <Link href={YOUTUBE_URL}>
-                    <Image src={youtube} alt="youtube" className="" />
-                  </Link>
-                </li>
-                <li className="pr-3">
-                  <Link href={INSTAGRAM_URL}>
-                    <Image src={instagram} alt="instagram" className="" />
-                  </Link>
-                </li>
-                <li>
-                  <Link href="https://www.yelp.com/biz/luskin-orthopaedic-institute-for-children-los-angeles">
-                    <Image src={yelp} alt="yelp" className="" />
-                  </Link>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
 
         {/* right section of footer */}
-        <div id="footer-right-content-container" className="hidden lg:flex">
+        <div
+          id="footer-right-content-container"
+          className="hidden md:flex flex-row"
+        >
           {/* left section of footer-right */}
           <div id="about-support-container" className="pr-24">
             <span id="about-container" className="flex flex-col pb-4">
