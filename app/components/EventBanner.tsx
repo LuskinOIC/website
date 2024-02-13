f2d7a0b638d4954d85c1333c2acb36c801a3c441
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-13 14:40:09 -0800

    Adjust for mobile

--- app/components/EventBanner.tsx
+++ app/components/EventBanner.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import renderRichTextToReactComponent from "../utils/rich-text";
 
 const styles = {
-  bannerSection: "flex-1/3 flex justify-center items-center",
-  wrapper: "w-full bg-gray-200 h-48 px-5 flex items-center",
-  container: "flex flex-row w-4/5 mx-auto justify-evenly",
+  wrapper: "w-full md:h-48",
+  container:
+    "flex flex-col py-4 gap-y-2  md:flex-row md:w-4/5 h-full mx-auto justify-evenly items-center",
+  bannerSection: "flex-1/3 flex justify-center",
+  btnContainer: "px-6 py-3 text-white rounded-lg",
 };
 
 export default function EventBanner({ section }: { section: PageSectionType }) {
@@ -15,22 +17,22 @@ export default function EventBanner({ section }: { section: PageSectionType }) {
   // console.log(eventBanner);
 
   return (
-    <section className={styles.wrapper}>
+    <section className={`${styles.wrapper} bg-[#0076AD] text-white`}>
       <div className={styles.container}>
         <Image
           className={styles.bannerSection}
           src={eventBannerImage.fields.file.url}
           alt={eventBanner.fields.title}
-          width={180}
-          height={180}
+          width={212}
+          height={120}
         />
-        <div className={`${styles.bannerSection}`}>
+        <div className={`hidden md:block ${styles.bannerSection}`}>
           {renderRichTextToReactComponent(eventBanner.fields.content)}
         </div>
-        <div className={styles.bannerSection}>
+        <div className={`${styles.bannerSection}`}>
           <Link
             href={eventBanner.fields.buttonLink}
-            className={`${styles.bannerSection} px-6 py-2 bg-blue-500 text-white rounded-lg`}
+            className={`${styles.bannerSection} ${styles.btnContainer} bg-blue-500 `}
           >
             VIEW EVENT
           </Link>
