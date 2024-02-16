c8e439f6617923399506da8eac0a215b51590ce9
Joe Torreggiani <joetorreggiani@gmail.com>
2024-02-15 16:00:06 -0800

    Make linter happy.

--- app/utils/contentful.ts
+++ app/utils/contentful.ts
@@ -20,7 +20,7 @@ const client = createClient({
 
 export async function getPageByType(
   pageType: string,
-  include: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 = 4
+  include: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 = 4,
 ) {
   const entry = await client.getEntries({
     content_type: "page",
@@ -142,7 +142,7 @@ export async function getLeadershipBioBySlug(slug: string) {
 
 /* PATIENT BIOS */
 export async function getPatientStories(
-  numberOfEntries: number | "all" = "all"
+  numberOfEntries: number | "all" = "all",
 ) {
   let query = {
     content_type: "patientBio",
