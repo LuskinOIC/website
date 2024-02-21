4545c19dc5a86516184e04d65e52093afd2f5780
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-21 13:31:26 -0800

    Add GlossarySection divisions

--- app/conditions/ConditionsIndexLayout.tsx
+++ app/conditions/ConditionsIndexLayout.tsx
@@ -13,8 +13,64 @@ export type ConditionsType = {
   learnMore: PageSectionType[];
 };
 
+type GroupedConditions = {
+  [key: string]: ConditionsType[];
+};
+
 export default async function ConditionsIndexLayout() {
   const conditions = (await getConditionTerms()) as unknown as ConditionsType[];
+
+  function groupTermsByFirstLetter(
+    conditions: ConditionsType[],
+  ): GroupedConditions {
+    return conditions.reduce((acc, condition) => {
+      const firstLetter = condition.term[0].toLowerCase();
+      if (!acc[firstLetter]) {
+        acc[firstLetter] = [];
+      }
+      acc[firstLetter].push(condition);
+      return acc;
+    }, {} as GroupedConditions);
+  }
+
+  const groupedConditions = groupTermsByFirstLetter(conditions);
+
+  const GlossarySection = () => {
+    return (
+      <>
+        {Object.entries(groupedConditions).map(([key, conditions], index) => (
+          <div key={index}>
+            <h1>{key.toUpperCase()}</h1>
+            {conditions.map((condition: ConditionsType) => (
+              <div
+                key={condition.slug}
+                id={condition.term}
+                className="grid grid-cols-5 border rounded px-[27px] py-[30px]"
+              >
+                <div className="col-span-4 flex flex-col gap-y-3 px-2">
+                  <ConditionDetails condition={condition} />
+                  {condition.relatedSpecialties && (
+                    <RelatedSpecialtiesComponent
+                      relatedSpecialties={condition.relatedSpecialties}
+                    />
+                  )}
+                </div>
+
+                <div className="col-span-1 self-end">
+                  <Button
+                    text="Learn More"
+                    href={`/conditions/${condition.slug}/learn-more`}
+                    className="uppercase"
+                    variant="bluePrimary"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </>
+    );
+  };
 
   return (
     <main className="flex flex-cols gap-6 w-5/6 mx-auto">
@@ -27,33 +83,7 @@ export default async function ConditionsIndexLayout() {
           </button>
         ))}
       </div>
-      <div className="grid grid-col gap-5 w-[95%]">
-        {conditions.map((condition) => (
-          <div
-            key={condition.slug}
-            id={condition.term}
-            className="grid grid-cols-5 border rounded px-[27px] py-[30px]"
-          >
-            <div className="col-span-4 flex flex-col gap-y-3 px-2">
-              <ConditionDetails condition={condition} />
-              {condition.relatedSpecialties && (
-                <RelatedSpecialtiesComponent
-                  relatedSpecialties={condition.relatedSpecialties}
-                />
-              )}
-            </div>
-
-            <div className="col-span-1 self-end">
-              <Button
-                text="Learn More"
-                href={`/conditions/${condition.slug}/learn-more`}
-                className="uppercase"
-                variant="bluePrimary"
-              />
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className="grid grid-col gap-5 w-[95%]">{GlossarySection()}</div>
     </main>
   );
 }
@@ -83,26 +113,6 @@ export default async function ConditionsIndexLayout() {
 //         <h2>Section 2</h2>
 //         <p>This is the content of Section 2.</p>
 //       </div>
-//     </div>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <h1>Page Title</h1>
-// <nav>
-//   <a href="#section1">Go to Section 1</a>
-//   <a href="#section2">Go to Section 2</a>
-// </nav>
-// <div id="section1" className="scroll-mt-5">
-//   <h2>Section 1</h2>
-//   <p>This is the content of Section 1.</p>
-// </div>
-// <div id="section2" className="scroll-mt-5">
-//   <h2>Section 2</h2>
-//   <p>This is the content of Section 2.</p>
-// </div>
 //     </div>
 //   );
 // }
