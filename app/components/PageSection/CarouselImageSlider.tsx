5dc689a9a5bae0f68816fcf587a2ed82939ee06c
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-23 10:31:16 -0800

    Add title option

--- app/components/PageSection/CarouselImageSlider.tsx
+++ app/components/PageSection/CarouselImageSlider.tsx
@@ -3,21 +3,19 @@
 import Image from "next/image";
 // Types
 import { CarouselSlideType } from "@/app/constants/types";
-import renderRichTextToReactComponent from "@/app/utils/rich-text";
 // Components
 import Slider from "@/app/components/Slider";
 import Button from "../ui/Button";
+import { Title1 } from "../ui/Typography/Title";
+import renderRichTextToReactComponent from "@/app/utils/rich-text";
 
 type CarouselImageLayoutProps = {
-  section: CarouselSlideType;
+  section: CarouselSlideType[];
 };
 
 export default function CarouselImageSlider({
   section,
 }: CarouselImageLayoutProps) {
-  // const { image, overlayAlignment, overlayContent, overlayButton } =
-  //   section[0].fields
-
   return (
     <section>
       <Slider
@@ -25,23 +23,26 @@ export default function CarouselImageSlider({
           section.map((slide, i) => (
             <div
               key={i}
-              className="relative mb-4 md:mb-10 w-full min-h-[40vh] md:min-h-[560px] text-left"
+              className="relative mb-4 md:mb-10 md:overflow-hidden w-full max-h-40  md:min-h-[560px] text-left"
             >
               <Image
                 src={slide.fields.image.fields.file.url}
                 alt={slide.fields.image.fields.file.fileName}
                 width={slide.fields.image.fields.file.details.image.width}
                 height={slide.fields.image.fields.file.details.image.height}
-                className="absolute inset-0 bg-cover bg-center"
               />
-
-              {/* Overlay Text Box */}
-              {slide.fields.overlayContent && (
-                <div className="absolute left-0 top-[25%] left-[10%] max-w-[500px] transform p-4 bg-white bg-opacity-75 rounded  py-2">
-                  {renderRichTextToReactComponent(slide.fields.overlayContent)}
+              {(slide.fields.overlayContent || slide.fields.title) && (
+                <div className="absolute left-0 top-[25%] left-[10%] max-w-[500px] transform bg-white bg-opacity-75 rounded  p-6">
+                  {slide.fields.title && (
+                    <Title1 className="lg:text-[48px] font-normal leading-snug">
+                      {slide.fields.title}
+                    </Title1>
+                  )}
+                  {slide.fields.overlayContent &&
+                    renderRichTextToReactComponent(slide.fields.overlayContent)}
                   {slide.fields.overlayButton && (
                     <Button
-                      className="w-full sm:w-auto text-center gap-2"
+                      className="w-full sm:w-auto text-center my-2 mx-0"
                       key={slide.fields.overlayButton.sys.id}
                       href={slide.fields.overlayButton.fields.buttonUrl}
                       isExternal={
@@ -53,9 +54,6 @@ export default function CarouselImageSlider({
                   )}
                 </div>
               )}
-
-              {/* Ensuring the overlay and background cover the same area */}
-              <div className="w-full h-full"></div>
             </div>
           )) as any
         }
