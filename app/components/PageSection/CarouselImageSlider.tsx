a10b2713f7c6c1a425b3cfd4c1c103dc214be3e5
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-23 14:03:33 -0800

    DRY code

--- app/components/PageSection/CarouselImageSlider.tsx
+++ app/components/PageSection/CarouselImageSlider.tsx
@@ -12,90 +12,61 @@ type CarouselImageLayoutProps = {
   section: CarouselSlideType[];
 };
 
+const styles = {
+  container:
+    "flex flex-col w-full h-[275px] md:block md:relative md:min-h-[560px] md:overflow-hidden mb-4 md:mb-10 md:text-left",
+  image: "h-full",
+  btn: "md:w-full text-center my-2 md:mx-0",
+  overlayDesktop:
+    "md:absolute md:left-0 md:top-[25%] md:left-[7%] md:max-w-[500px] md:transform bg-white md:bg-opacity-75 md:rounded md:p-6",
+  title: "font-normal lg:text-[48px] leading-snug",
+};
+
+const OverlayComponent = ({ overlayTitle, overlayButton, btnStyle }: any) => (
+  <>
+    {overlayTitle && <Title1 className={styles.title}>{overlayTitle}</Title1>}
+    {overlayButton && (
+      <Button
+        className={btnStyle}
+        key={overlayButton.sys.id}
+        href={overlayButton.fields.buttonUrl}
+        isExternal={overlayButton.fields.externalLink}
+        text={overlayButton.fields.text}
+        variant={overlayButton.fields.type}
+      />
+    )}
+  </>
+);
+
 export default function CarouselImageSlider({
   section,
 }: CarouselImageLayoutProps) {
   return (
-    <>
-      <section className="hidden md:block">
-        <Slider
-          slides={
-            section.map((slide, i) => (
-              <div
-                key={i}
-                className="relative mb-4 md:mb-10 md:overflow-hidden w-full max-h-40  md:min-h-[560px] text-left"
-              >
-                <Image
-                  src={slide.fields.image.fields.file.url}
-                  alt={slide.fields.image.fields.file.fileName}
-                  width={slide.fields.image.fields.file.details.image.width}
-                  height={slide.fields.image.fields.file.details.image.height}
-                />
-                {slide.fields.overlayTitle && (
-                  <div className="absolute left-0 top-[25%] left-[10%] max-w-[500px] transform bg-white bg-opacity-75 rounded p-6">
-                    {slide.fields.overlayTitle && (
-                      <Title1 className="lg:text-[48px] font-normal leading-snug">
-                        {slide.fields.overlayTitle}
-                      </Title1>
-                    )}
-                    {slide.fields.overlayButton && (
-                      <Button
-                        className="w-[200px] sm:w-auto text-center my-2"
-                        key={slide.fields.overlayButton.sys.id}
-                        href={slide.fields.overlayButton.fields.buttonUrl}
-                        isExternal={
-                          slide.fields.overlayButton.fields.externalLink
-                        }
-                        text={slide.fields.overlayButton.fields.text}
-                        variant={slide.fields.overlayButton.fields.type}
-                      />
-                    )}
-                  </div>
-                )}
-              </div>
-            )) as any
-          }
-        />
-      </section>
-
-      <section className="md:hidden">
-        <Slider
-          slides={
-            section.map((slide, i) => (
-              <div key={i} className="flex flex-col mb-4 h-[275px]">
-                <Image
-                  src={slide.fields.image.fields.file.url}
-                  alt={slide.fields.image.fields.file.fileName}
-                  width={slide.fields.image.fields.file.details.image.width}
-                  height={slide.fields.image.fields.file.details.image.height}
-                  className="h-full"
-                />
-                {(slide.fields.overlayTitle || slide.fields.overlayButton) && (
-                  <div>
-                    {slide.fields.overlayTitle && (
-                      <Title1 className="font-normal ">
-                        {slide.fields.overlayTitle}
-                      </Title1>
-                    )}
-                    {slide.fields.overlayButton && (
-                      <Button
-                        className="text-center my-2 "
-                        key={slide.fields.overlayButton.sys.id}
-                        href={slide.fields.overlayButton.fields.buttonUrl}
-                        isExternal={
-                          slide.fields.overlayButton.fields.externalLink
-                        }
-                        text={slide.fields.overlayButton.fields.text}
-                        variant={slide.fields.overlayButton.fields.type}
-                      />
-                    )}
-                  </div>
-                )}
-              </div>
-            )) as any
-          }
-        />
-      </section>
-    </>
+    <section>
+      <Slider
+        slides={
+          section.map((slide, i) => (
+            <div key={i} className={styles.container}>
+              <Image
+                src={slide.fields.image.fields.file.url}
+                alt={slide.fields.image.fields.file.fileName}
+                width={slide.fields.image.fields.file.details.image.width}
+                height={slide.fields.image.fields.file.details.image.height}
+                className={styles.image}
+              />
+              {(slide.fields.overlayTitle || slide.fields.overlayButton) && (
+                <div className={styles.overlayDesktop}>
+                  <OverlayComponent
+                    overlayTitle={slide.fields.overlayTitle}
+                    overlayButton={slide.fields.overlayButton}
+                    btnStyle={styles.btn}
+                  />
+                </div>
+              )}
+            </div>
+          )) as any
+        }
+      />
+    </section>
   );
 }
