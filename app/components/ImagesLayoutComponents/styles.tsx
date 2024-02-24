461e69cb2fa4afee5cba1b826879320edcb335c9
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-23 16:10:00 -0800

    Correct desktop button size

--- app/components/ImagesLayoutComponents/styles.tsx
+++ app/components/ImagesLayoutComponents/styles.tsx
@@ -2,7 +2,7 @@ export const styles = {
   container:
     "flex flex-col w-full h-[260px] md:block md:relative md:min-h-[560px] md:overflow-hidden mb-4 md:mb-10 md:text-left",
   image: "h-full",
-  btn: "md:w-full text-center my-2 md:mx-0",
+  btn: "md:w-auto text-center my-2 md:mx-0",
   overlayDesktop:
     "md:absolute md:left-0 md:top-[25%] md:left-[7%] md:max-w-[500px] md:transform bg-white md:bg-opacity-75 md:rounded md:p-6",
   title: "font-normal lg:text-[48px] leading-snug",
