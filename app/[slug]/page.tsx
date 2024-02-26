23142f878d2144e40d6f2dfa1653be7645216fb9
Joe Torreggiani <joetorreggiani@gmail.com>
2024-02-25 17:07:36 -0800

    Enable dynamic meta data.

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -10,6 +10,30 @@ export async function generateStaticParams() {
     .map((page) => ({ slug: page.slug }));
 }
 
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const pages = await getPages();
+  const page = pages.find((page) => page.slug === params.slug);
+
+  if (!page) {
+    return {
+      title: "Page Not Found",
+    };
+  }
+
+  return {
+    title: page.pageType,
+  };
+}
+
 export default async function AppPage({
   params,
 }: {
