96a6b271c2b47e40a10ae62c42d88c685daa4395
Yoline <92892101+YolineB@users.noreply.github.com>
2024-02-28 10:43:35 -0800

    Add specialty color bg

--- app/conditions/RelatedSpecialties.tsx
+++ app/conditions/RelatedSpecialties.tsx
@@ -1,23 +1,31 @@
 import Link from "next/link";
+import { SPECIALTY_COLORS } from "@/app/constants/specialtyColors";
 import { SpecialtyType } from "@/app/constants/types";
 
-const RelatedSpecialtiesComponent = ({ relatedSpecialties }: any) => {
-  // const colors = ['#FFB6C4', '#98D4C9', '#FFEEAE', '#EBBBAB', '#C9DA95', '#B99DD0', '#B9E0EB', '#C4ED4B'];
-  // const colorIndex = index % colors.length;
-  // const backgroundColor = colors[colorIndex];
-  // console.log(backgroundColor)
-  // console.log("background Color", colors)
+const styles = {
+  container: "flex flex-row items-center pt-6 space-x-2",
+  defaultBgColor: "#D3D3D3",
+  linkContainer: "hover:underline px-4 py-1 text-black rounded-full",
+};
 
+const RelatedSpecialtiesComponent = ({
+  relatedSpecialties,
+}: {
+  relatedSpecialties: SpecialtyType[];
+}) => {
   return (
-    <div className="flex flex-row pt-6">
+    <div className={styles.container}>
       <p>Related Specialties:</p>
-      {relatedSpecialties.map((specialty: SpecialtyType) => {
+      {relatedSpecialties.map((specialty) => {
+        const backgroundColor =
+          SPECIALTY_COLORS[specialty.fields.slug] || styles.defaultBgColor;
         return (
           <Link
             key={specialty.fields.name}
             href={`/patient-care/specialties/${specialty.fields.slug}`}
             passHref
-            className={` px-4`}
+            style={{ backgroundColor }}
+            className={styles.linkContainer}
           >
             {specialty.fields.name}
           </Link>
