cadc204298751c1edcfcd0cff8bde90cd187cbdf
Joe Torreggiani <joetorreggiani@gmail.com>
2024-03-05 15:12:37 -0500

    Add redirect for paths saved on Google results.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,36 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/about-oic/",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/patient-care/specialties/urgent-care/",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/contact-locations/",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/about-oic/careers/",
+        destination:
+          "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=1fe50326-beac-4a7c-a564-f8a14f9c5bb0&ccId=19000101_000001&type=MP&lang=en_US",
+        permanent: true,
+      },
+      {
+        source: "/ways-to-give/volunteer/",
+        destination: "/volunteer",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 if (process.env.EXPORT_STATIC === "true") {
