090d6e7bab2e3b1227a5f9c09841d47e487a6157
Joe Torreggiani <joetorreggiani@gmail.com>
2024-03-11 18:52:48 -0400

    Automatically generate sitemap.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,54 +1,6 @@
 import { MetadataRoute } from "next";
+import sitemapData from "./sitemap.json";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://luskinoic.org",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: "https://luskinoic.org/about/",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
-    {
-      url: "https://luskinoic.org/patient-care/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: "https://luskinoic.org/patient-care/specialties/urgent-care/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    },
-    {
-      url: "https://luskinoic.org/medical-professionals",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://luskinoic.org/physicians/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://luskinoic.org/patient-care/specialties/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://luskinoic.org/ways-to-give/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-  ];
+  return sitemapData as MetadataRoute.Sitemap;
 }
