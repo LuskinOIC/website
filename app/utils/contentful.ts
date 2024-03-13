dfdbc77002a603cad5cb18d5d6533a8f52634d89
Joe Torreggiani <joetorreggiani@gmail.com>
2024-03-12 21:15:16 -0400

    Add Contentful host param. (#232)

--- app/utils/contentful.ts
+++ app/utils/contentful.ts
@@ -14,6 +14,7 @@ import {
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID as string,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  host: process.env.CONTENTFUL_HOST || "cdn.contentful.com",
 });
 
 /* PAGE */
