e2fdec7c43ab32e356b5f9e6f183c59a9b2add66
Yoline <92892101+YolineB@users.noreply.github.com>
2024-03-14 22:19:44 -0700

    Separate widget section

--- app/components/NabarLayout/NavbarNew.tsx
+++ app/components/NabarLayout/NavbarNew.tsx
@@ -17,7 +17,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import externalIconWhite from "@/public/external-link-icon-white.svg";
 import externalIconBlack from "@/public/external-link-icon-black.svg";
-import { MYCHART_URL, SAVE_MY_SPOT } from "@/app/constants/links";
+import SupportWidget from "./SupportWidget";
 
 const styles = {
   navigationMenu: (isScrolled: boolean) =>
@@ -26,34 +26,23 @@ const styles = {
         ? "transition-all duration-300 ease-out md:h-[102px]"
         : "transition-all duration-300 ease-out md:h-[180px]"
     }`,
-  // desktopContainer: 'flex flex-col w-full h-full items-center ',
-  // topContainer: 'z-10 w-full relative top-0 right-0',
-  // middleContainer:
-  //   'relative z-50 flex flex-row w-full h-full items-center gap-x-40 bg-yellow-200',
   logoContainer: (isScrolled: boolean) =>
-    `hidden md:block w-fit h-fit bg-pink-500 ${isScrolled ? "py-2" : "py-4"}`,
+    `hidden md:block w-fit h-fit h-fit ml-20 bg-pink-500 ${
+      isScrolled ? "py-2" : "py-4"
+    }`,
   logoImage: (isScrolled: boolean) =>
     `${
       isScrolled
         ? "transition-all duration-300 ease-out w-24"
         : "transition-all duration-300 ease-out w-30"
     }`,
-  // secondaryMenuContainer: 'w-full h-full bg-yellow-200',
-  widgetContainer: (isScrolled: boolean) =>
-    `flex flex-row gap-10 justify-end w-full pr-8 text-white bg-[#0076AD] h-10  text-base transition-transform ease-out ${
-      isScrolled ? "transform -translate-y-full" : "transform translate-y-0"
-    }`,
-  overlapTab: (isScrolled: boolean) =>
-    `hidden md:flex flex-col text-center justify-center z-50 px-7 py-4 rounded-b-2xl hover:underline transition-all duration-500 ease-in-out ${
-      isScrolled ? "max-h-0 overflow-hidden" : "max-h-[5rem] h-20"
-    }`,
   mainNavScrollWrapper: (isScrolled: boolean) =>
     `w-full z-50 md:flex  h-full transition-opacity duration-500 ease-out ${
       isScrolled ? "absolute inset-x-0 top-0" : ""
     }`,
-  infoList:
-    "hidden md:flex h-full bg-luskin-brightBlue text-black text-base font-medium py-1 px-3",
-  listItem: "mr-4 underline underline-offset-4 hover:text-slate-200",
+  // infoList:
+  //   "hidden md:flex h-full bg-luskin-brightBlue text-black text-base font-medium py-1 px-3",
+  // listItem: "mr-4 underline underline-offset-4 hover:text-slate-200",
 };
 
 export default function Navbar({
@@ -90,30 +79,10 @@ export default function Navbar({
 
   return (
     <NavigationMenu className={styles.navigationMenu(isScrolled)}>
-      <div className={`${styles.widgetContainer(isScrolled)}`}>
-        <div className={`${styles.overlapTab(isScrolled)} bg-[#825AA4]`}>
-          Urgent Care <strong>Save My Spot</strong>
-        </div>
-        <div className="py-2">(213) 742 - 1000</div>
-        <div className="py-2"> | </div>
-        <a
-          href="/espanol"
-          onClick={() => handleClick("Nav Spanish Page")}
-          className="py-2 font-semibold hover:underline"
-        >
-          Español
-        </a>
-        <div
-          className={`${styles.overlapTab(
-            isScrolled,
-          )} bg-[#FCE385] text-black font-semibold text-lg`}
-        >
-          DONATE
-        </div>
-      </div>
+      <SupportWidget isScrolled={isScrolled} />
 
-      <div className={`${styles.mainNavScrollWrapper(isScrolled)}`}>
-        <div className={`h-fit ml-20 ${styles.logoContainer(isScrolled)}`}>
+      <div className={styles.mainNavScrollWrapper(isScrolled)}>
+        <div className={styles.logoContainer(isScrolled)}>
           <Link href="/" onClick={() => handleClick("Logo Home")}>
             <Image
               className={styles.logoImage(isScrolled)}
@@ -124,6 +93,7 @@ export default function Navbar({
             />
           </Link>
         </div>
+
         <div className="flex flex-row justify-evenly w-full items-center">
           <div className="item-end">LOGO</div>
           <div className="item-end">LOGO</div>
---
+++ app/components/NabarLayout/SupportWidget.tsx
@@ -0,0 +1,60 @@
+import { SAVE_MY_SPOT } from "@/app/constants/links";
+import { sendGAEvent } from "@next/third-parties/google";
+
+const styles = {
+  widgetContainer: (isScrolled: boolean) =>
+    `hidden md:flex flex-row gap-10 justify-end w-full pr-8 text-white bg-[#0076AD] h-10 text-base transition-transform ease-out ${
+      isScrolled ? "transform -translate-y-full" : "transform translate-y-0"
+    }`,
+  overlapTab: (isScrolled: boolean) =>
+    `hidden md:flex flex-col text-center justify-center z-50 px-7 py-4 rounded-b-2xl hover:underline transition-all duration-500 ease-in-out ${
+      isScrolled ? "max-h-0 overflow-hidden" : "max-h-[5rem] h-20"
+    }`,
+};
+
+type SupportWidgetProps = {
+  isScrolled: boolean;
+};
+
+const handleClick = (text: string) => {
+  sendGAEvent({
+    event: "buttonClicked",
+    value: text,
+  });
+};
+
+export default function SupportWidget({ isScrolled }: SupportWidgetProps) {
+  return (
+    <div className={`${styles.widgetContainer(isScrolled)}`}>
+      <a
+        href={SAVE_MY_SPOT}
+        onClick={() => handleClick("Nav Save My Spot")}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${styles.overlapTab(isScrolled)} bg-[#825AA4]`}
+      >
+        Urgent Care <strong>Save My Spot</strong>
+      </a>
+      <div className="py-2">(213) 742 - 1000</div>
+      <div className="py-2"> | </div>
+      <a
+        href="/espanol"
+        onClick={() => handleClick("Nav Spanish Page")}
+        className="py-2 font-semibold hover:underline"
+      >
+        Español
+      </a>
+      <a
+        href={SAVE_MY_SPOT}
+        onClick={() => handleClick("Nav Save My Spot")}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${styles.overlapTab(
+          isScrolled,
+        )} bg-[#FCE385] text-black font-semibold text-lg`}
+      >
+        DONATE
+      </a>
+    </div>
+  );
+}
