ccbeeb8ee7f733ad8b11768a6d36119df1a4c6c9
Yoline <92892101+YolineB@users.noreply.github.com>
2024-03-22 16:49:14 -0700

    Format results

--- app/components/NabarLayout/SearchForm.tsx
+++ app/components/NabarLayout/SearchForm.tsx
@@ -7,14 +7,18 @@ import { searchIndex } from "./searchIndex";
 const styles = {
   input: "w-full pl-4 pr-20 py-2 rounded-l-md border border-r-0 bg-white",
   inputResults:
-    "w-full mt-3 pl-4 pr-20 py-2 rounded-l-md border border-r-0 bg-white",
+    "w-full mt-3 rounded-md border-2 border-[#0076AD] bg-white flex flex-col",
   searchBtn:
     "px-4 py-2 bg-[#0076AD] text-white rounded-r-md border border-[#1868F1]",
+  result:
+    "p-4 border-b border-[#0076AD] last:border-b-0 text-[#171515] font-bold text-lg hover:underline underline-offset-4",
 };
 
 const SearchForm = () => {
-  const [searchResults, setSearchResults] = useState<string[]>([]);
-  console.log(searchResults);
+  const [searchResults, setSearchResults] = useState<
+    Array<{ path: string; title: string }>
+  >([]);
+  let results: any = [];
 
   function handleSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
     const query = String(e.target.value).toLowerCase().trim();
@@ -24,16 +28,15 @@ const SearchForm = () => {
     );
     const exactMatch = Object.keys(searchIndex).filter((key) => key === query);
 
-    let results: string[] = [];
-
     if (exactMatch.length > 0) {
-      results = searchIndex[query as keyof typeof searchIndex].map(
-        (item) => item.path,
-      );
+      results = searchIndex[query as keyof typeof searchIndex].map((item) => ({
+        path: item.path,
+        title: item.title,
+      }));
     } else if (partialMatch.length > 0) {
       results = partialMatch
         .flatMap((key) => searchIndex[key as keyof typeof searchIndex])
-        .map((item) => item.path);
+        .map((item) => ({ path: item.path, title: item.title }));
     }
 
     setSearchResults(Array.from(new Set(results)));
@@ -54,9 +57,9 @@ const SearchForm = () => {
       {searchResults.length > 0 && (
         <div id="search" className={styles.inputResults}>
           {searchResults.map((result, i) => (
-            <div key={i}>
-              <Link href={result}>{result}</Link>
-            </div>
+            <Link key={i} href={result.path} className={styles.result}>
+              {result.title}
+            </Link>
           ))}
         </div>
       )}
