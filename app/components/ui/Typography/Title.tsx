783896fdefa5cfccc094e531846cd218c1f3a7cd
Joe Torreggiani <joetorreggiani@gmail.com>
2024-03-24 19:23:00 -0400

    Support anchor tags on conditional page. (#251)

--- app/components/ui/Typography/Title.tsx
+++ app/components/ui/Typography/Title.tsx
@@ -9,6 +9,7 @@ type TitleProps = {
   titleSize?: string;
   bold?: boolean;
   luskinHeader?: boolean;
+  id?: string;
 };
 
 export const myFont = localFont({ src: "./Mathlete-Bulky.otf" });
@@ -22,12 +23,16 @@ export const LuskinHeader = ({ children, className = "" }: TitleProps) => {
   return <h1 className={classes}>{children}</h1>;
 };
 
-export const Title1 = ({ children, className = "" }: TitleProps) => {
+export const Title1 = ({ id, children, className = "" }: TitleProps) => {
   const classes = cn(
     "text-[28px] md:text-[32px] leading-10 py-2 font-bold",
     className,
   );
-  return <h1 className={classes}>{children}</h1>;
+  return (
+    <h1 id={id} className={classes}>
+      {children}
+    </h1>
+  );
 };
 
 export const Title2 = ({ children, className = "" }: TitleProps) => {
--- app/conditions/ConditionsIndexLayout.tsx
+++ app/conditions/ConditionsIndexLayout.tsx
@@ -38,7 +38,7 @@ export default async function ConditionsIndexLayout() {
     return (
       <>
         {Object.entries(groupedConditions).map(([key, conditions], index) => (
-          <div key={index}>
+          <div key={index} id={key.toUpperCase()} className="scroll-mt-[100px]">
             <Title1>{key.toUpperCase()}</Title1>
             {conditions.map((condition: ConditionsType) => (
               <div key={condition.term}>
@@ -55,13 +55,13 @@ export default async function ConditionsIndexLayout() {
     <section className={styles.conditionsContainer}>
       {/* TO DO: Search + Glossary Index BTN */}
       {/* <SearchBar /> */}
-      {/* <div className={styles.lettersContainer}>
+      <div className={styles.lettersContainer}>
         {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => (
-          <button key={letter} className={styles.letter}>
+          <a href={`#${letter}`} key={letter} className={styles.letter}>
             {letter}
-          </button>
+          </a>
         ))}
-      </div> */}
+      </div>
       <div className={styles.termsContainer}>{GlossarySection()}</div>
     </section>
   );
