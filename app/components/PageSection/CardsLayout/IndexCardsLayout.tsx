d21e4d2e4fd2da163ff02b5339e73286c60ee83c
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-12 00:07:43 -0700

    Add mobile button/logic

--- app/components/PageSection/CardsLayout/IndexCardsLayout.tsx
+++ app/components/PageSection/CardsLayout/IndexCardsLayout.tsx
@@ -13,14 +13,15 @@ import renderRichTextToReactComponent, {
 import Button from "@/app/components/ui/Button";
 
 const styles = {
-  sectionContainer:
-    "grid md:grid-cols-2 gap-6 md:m-auto justify-items-center pt-2 pb-12 md:px-32",
+  desktopSectionContainer:
+    "hidden md:grid md:grid-cols-2 gap-6 md:m-auto justify-items-center pt-2 pb-12 md:px-32",
+  mobileSectionContainer: "grid justify-items-center md:hidden  py-4 px-5",
   cardContainer:
     "flex flex-col h-full col-span-1 rounded-[10px] pb-8 md:p-10 overflow-hidden mx-6 md:mx-0",
   contentContainer: "flex flex-col h-full px-5 md:px-0",
   title: "text-[#0076AD] md:leading-[30px]",
   contentDetails: "flex flex-col justify-apart gap-2 h-full",
-  button: "mt-auto text-center",
+  button: "w-full md:w-fit mt-auto text-center",
   borderGray: "shadow-lg border border-black border-opacity-10",
   borderGreen: "border-2 border-[#99C221]",
 };
@@ -29,10 +30,13 @@ const descriptionClassNames: ClassNames = {
   paragraph: "py-2 text-base font-normal md:text-xl md:leading-[30px] pb-4",
 };
 
-export function InfoCardContent(cardContent: TextType, i: number) {
+export function InfoCardContent(
+  cardContent: TextType,
+  i: number,
+  pathname: string,
+) {
   const [isCurrentPage, setCurrentPage] = useState<boolean>(true);
   const { title, content, button } = cardContent.fields;
-  const pathname = usePathname();
 
   useEffect(() => {
     const formattedButtonUrl = button?.fields.buttonUrl.replace(/\/$/, "");
@@ -70,13 +74,39 @@ export function InfoCardContent(cardContent: TextType, i: number) {
 }
 
 export default function IndexCardsLayout({ section }: { section: CardType[] }) {
+  const pathname = usePathname();
+
+  const mobileButton = section.find((card) => {
+    const buttonUrl =
+      card.fields.cardContent?.fields.button?.fields.buttonUrl.replace(
+        /\/$/,
+        "",
+      );
+    return buttonUrl !== pathname.replace(/\/$/, "");
+  })?.fields.cardContent?.fields.button;
+
   return (
-    <section className={styles.sectionContainer}>
-      {section.map((card, i) => {
-        return (
-          card.fields.cardContent && InfoCardContent(card.fields.cardContent, i)
-        );
-      })}
-    </section>
+    <>
+      <section className={styles.desktopSectionContainer}>
+        {section.map((card, i) => {
+          return (
+            card.fields.cardContent &&
+            InfoCardContent(card.fields.cardContent, i, pathname)
+          );
+        })}
+      </section>
+      <section className={styles.mobileSectionContainer}>
+        {mobileButton && (
+          <Button
+            className={styles.button}
+            key={mobileButton.sys.id}
+            href={mobileButton.fields.buttonUrl}
+            isExternal={mobileButton.fields.externalLink}
+            text={`View ${mobileButton.fields.text}`}
+            variant="blueSecondary"
+          />
+        )}
+      </section>
+    </>
   );
 }
