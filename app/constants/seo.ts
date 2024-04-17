3bce876486c3968f1f333a4beab74c7664389460
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-17 12:20:48 -0700

    Landing Page SEO Meta tag (#298)
    * Correct orthpaedic default
    
    * Add generateMetadata to landing

--- app/constants/seo.ts
+++ app/constants/seo.ts
@@ -1,6 +1,6 @@
 export const SEO_DEFAULTS = {
   TITLE:
-    "Pediatric Orthopedic Doctor - Luskin Orthopaedic Institute for Children",
+    "Pediatric Orthopaedic Doctor - Luskin Orthopaedic Institute for Children",
   DESCRIPTION:
     "Luskin Orthopaedic Institute for Children provides the highest levels of specialized care in pediatric orthopedics. Your child will receive exceptional care & treatment for their pediatric orthopedic needs.",
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import type { Metadata } from "next";
 import { getPageByType } from "@/app/utils/contentful";
 import { PAGE_TYPES } from "@/app/constants/entries";
 import Page from "@/app/components/Page";
+import { SEO_DEFAULTS } from "@/app/constants/seo";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const landingPage = await getPageByType(PAGE_TYPES.LANDING_PAGE);
+  const seoMetaTagFields = landingPage.seoMetaTagFields;
+
+  return {
+    title: seoMetaTagFields?.fields?.title || SEO_DEFAULTS.TITLE,
+    description:
+      seoMetaTagFields?.fields?.description || SEO_DEFAULTS.DESCRIPTION,
+  };
+}
 
 // This is the root page to the website.
 export default async function Home() {
