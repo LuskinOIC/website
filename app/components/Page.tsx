04b02b78dd4ffe1663c6db38cfdbd6596adf617f
jtorreggiani <joetorreggiani@gmail.com>
2024-04-23 23:32:06 -0400

    Protect against pages with no sections.

--- app/components/Page.tsx
+++ app/components/Page.tsx
@@ -7,6 +7,10 @@ interface PagePropsType {
 
 // A resuable component that renders a page with sections.
 export default function Page({ page }: PagePropsType) {
+  // We want to protect against bad intermediate states where the page or sections
+  // are not loaded.
+  if (!page || !page.pageSections) return null;
+
   return (
     <div>
       {page.pageSections.map((pageSection: PageSectionType) => (
