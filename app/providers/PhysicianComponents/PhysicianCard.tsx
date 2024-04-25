c33a4ac74f61ce06742d0e1a1252d057901a534b
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-24 23:06:39 -0700

    Extract styles

--- app/providers/PhysicianComponents/PhysicianCard.tsx
+++ app/providers/PhysicianComponents/PhysicianCard.tsx
@@ -4,19 +4,26 @@ import { Title3 } from "@/app/components/ui/Typography/Title";
 import Link from "next/link";
 import { getProviderFormattedPath } from "@/app/providers/PhysicianComponents/formattingProviderPath";
 
+const styles = {
+  cardContainer:
+    "md:min-h-[273px] bg-[#0076AD] rounded-lg md:border md:bg-card md:text-card-foreground",
+  interactiveStates: "shadow-md md:shadow-sm hover:shadow-lg",
+  contentContainer: "flex flex-row md:flex-col items-center",
+  image: "w-28 h-28 p-1.5 md:w-auto md:h-auto",
+  title: "md:px-2 md:py-4 text-center md:leading-none text-white md:text-black",
+};
+
 const CardContent = ({ physician }: { physician: PhysicianBioType }) => {
   return (
-    <div className="flex flex-row md:flex-col items-center ">
+    <div className={styles.contentContainer}>
       <Image
         src={physician.portrait.fields.file.url}
         width={219}
         height={273}
         alt={physician.portrait.fields.description}
-        className="w-28 h-28 p-1.5 md:w-auto md:h-auto"
+        className={styles.image}
       />
-      <Title3 className="md:px-2 md:py-4 text-center md:leading-none text-white md:text-black">
-        {physician.name}
-      </Title3>
+      <Title3 className={styles.title}>{physician.name}</Title3>
     </div>
   );
 };
@@ -30,12 +37,12 @@ export default function PhysicianCard({
   return shouldWrapWithLink ? (
     <Link
       href={getProviderFormattedPath(physician.providerType, physician.slug)}
-      className="md:min-h-[273px] bg-[#0076AD] rounded-lg md:border md:bg-card md:text-card-foreground shadow-md md:shadow-sm hover:shadow-lg"
+      className={`${styles.cardContainer} ${styles.interactiveStates}`}
     >
       <CardContent physician={physician} />
     </Link>
   ) : (
-    <div className="md:min-h-[273px] bg-[#0076AD] rounded-lg md:border md:bg-card md:text-card-foreground">
+    <div className={styles.cardContainer}>
       {" "}
       <CardContent physician={physician} />{" "}
     </div>
