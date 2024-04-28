2c9e3cbf80ee6b856155ef3019ea98b1b9a3c961
jtorreggiani <joetorreggiani@gmail.com>
2024-04-28 19:17:40 -0400

    Add missing blogs and insights.

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { BlogCardsRowType } from "@/app/constants/types";
 import BlogCardsRow from "@/app/components/BlogCardsRow";
 import { SEO_DEFAULTS } from "@/app/constants/seo";
 import PostComponent from "@/app/blog/components/PostComponent";
-
+import { redirect } from "next/navigation";
 interface PagePropsType {
   params: { slug: string };
 }
@@ -16,6 +16,10 @@ export async function generateMetadata({
 }: PagePropsType): Promise<Metadata> {
   const newsPost = await getNewsPostBySlug(params.slug);
 
+  if (newsPost === undefined) {
+    return {};
+  }
+
   return {
     title: `${newsPost.fields.title || SEO_DEFAULTS.TITLE} - LuskinOIC`,
     description: documentToPlainTextString(newsPost.fields.subTitle),
@@ -23,7 +27,9 @@ export async function generateMetadata({
 }
 
 export async function generateStaticParams() {
-  const newsPosts = await getNewsPosts();
+  let newsPosts = await getNewsPosts();
+
+  newsPosts = newsPosts.filter((newsPost) => newsPost.slug);
 
   return newsPosts.map((post) => ({ slug: post.slug }));
 }
@@ -35,6 +41,10 @@ export default async function NewsArticle({
 }) {
   const newsPost = await getNewsPostBySlug(params.slug);
   const news = (await getNewsPosts(4)) as unknown as BlogCardsRowType[];
+
+  if (!newsPost) {
+    redirect("/");
+  }
 
   return (
     <div className="flex flex-col">
