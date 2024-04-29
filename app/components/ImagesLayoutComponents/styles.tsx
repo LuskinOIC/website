0f82d1f2f20406990b1d0a69962494deaa10a4b4
Yoline <92892101+YolineB@users.noreply.github.com>
2024-04-29 13:18:22 -0700

    Adjust carousel and urgent care card size

--- app/components/ImagesLayoutComponents/styles.tsx
+++ app/components/ImagesLayoutComponents/styles.tsx
@@ -1,6 +1,6 @@
 export const styles = {
   container: "relative flex flex-col w-full overflow-hidden md:text-left",
-  image: "w-full h-auto aspect-ratio",
+  image: "w-full min-h-[275px] md:h-auto aspect-ratio",
   btn: "h-6 md:h-auto text-[10px] md:text-base md:w-auto text-center md:my-2 md:mx-0",
   overlayDesktop:
     "absolute top-1/2 -translate-x-1/2 -translate-y-1/2 w-auto md:max-w-[500px] bg-white bg-opacity-75 rounded p-6",
