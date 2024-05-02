f41a812195cbadaaca3ab5a88abc81ee806ea054
jtorreggiani <joetorreggiani@gmail.com>
2024-05-01 20:33:57 -0400

    Fix parsing error.

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/*",
+        source: "/api/draft/*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
