657def9b5c9037c28950cf72fb6648ff9be592b0
jtorreggiani <joetorreggiani@gmail.com>
2024-05-06 00:15:17 -0400

    Add page section styles to health information library.

--- app/conditions/[slug]/learn-more/page.tsx
+++ app/conditions/[slug]/learn-more/page.tsx
@@ -5,6 +5,7 @@ import { getConditionBySlug, getConditionTerms } from "@/app/utils/contentful";
 import RelatedSpecialtiesComponent from "@/app/conditions/RelatedSpecialties";
 import { Title3 } from "@/app/components/ui/Typography/Title";
 import { SEO_DEFAULTS } from "@/app/constants/seo";
+import { PageSectionContainer } from "@/app/components/PageSection/PageSection";
 
 export async function generateStaticParams() {
   const conditions = await getConditionTerms();
@@ -39,24 +40,26 @@ export default async function LearnMorePage({
   const condition = await getConditionBySlug(params.slug);
 
   return (
-    <section>
+    <>
       {condition.fields.learnMore ? (
-        <div>
+        <div className="mt-10">
           {condition.fields.learnMore.map((pageSection: PageSectionType) => (
             <PageSection key={pageSection.fields.title} section={pageSection} />
           ))}
         </div>
       ) : (
-        <div className="grid grid-col gap-5 p-5">
-          <Title3 className="text-[#0076AD]">{condition.fields.term}</Title3>
-          <p>{condition.fields.definition}</p>
-          {condition.fields.relatedSpecialties && (
-            <RelatedSpecialtiesComponent
-              relatedSpecialties={condition.fields.relatedSpecialties}
-            />
-          )}
-        </div>
+        <PageSectionContainer>
+          <div className="grid grid-col gap-5 p-5">
+            <Title3 className="text-[#0076AD]">{condition.fields.term}</Title3>
+            <p>{condition.fields.definition}</p>
+            {condition.fields.relatedSpecialties && (
+              <RelatedSpecialtiesComponent
+                relatedSpecialties={condition.fields.relatedSpecialties}
+              />
+            )}
+          </div>
+        </PageSectionContainer>
       )}
-    </section>
+    </>
   );
 }
