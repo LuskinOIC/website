00d70cfaab2899cd301db67434a431daa2b310fa
Yoline <92892101+YolineB@users.noreply.github.com>
2024-05-07 22:23:00 -0700

    Correct locations card tablet alignment

--- app/components/LocationsCard.tsx
+++ app/components/LocationsCard.tsx
@@ -10,73 +10,73 @@ type LocationsCardType = { locations: LocationType[] };
 
 export default function LocationsCard({ locations }: LocationsCardType) {
   return (
-    <div className="md:py-10">
-      <Card className="grid md:grid-cols-3 justify-items-center py-6 md:py-12">
-        {locations.map((location, index) => (
-          <LocationContent
-            key={index}
-            location={location}
-            lastLocation={index == locations.length - 1}
-          />
-        ))}
-      </Card>
-    </div>
+    <Card className="grid md:grid-cols-3 justify-items-center py-6 lg:py-12">
+      {locations.map((location, index) => (
+        <LocationContent
+          key={index}
+          location={location}
+          lastLocation={index == locations.length - 1}
+        />
+      ))}
+    </Card>
   );
 }
 
 type LocationContentType = { location: LocationType; lastLocation: boolean };
 
 function LocationContent({ location, lastLocation }: LocationContentType) {
-  let outerClassName = "flex flex-col w-full md:px-6 items-center mb-6";
+  let outerClassName = "h-full flex flex-col w-full lg:px-6 mb-6 items-center";
   if (!lastLocation) {
     outerClassName = `${outerClassName} border-b-[1px] md:border-b-0 md:border-r-[3px] border-luskin-green`;
   }
   return (
     <div className={outerClassName}>
-      <Title2 className="md:basis-1/6 md:text-xl leading-[30px] font-bold font-arial pt-0">
+      <Title2 className="md:h-[60px] md:leading-0 lg:text-lg font-bold font-arial pt-0 text-center">
         {location.fields.name}
       </Title2>
-      {location.fields.hours && (
-        <div className="mt-1">
-          {renderRichTextToReactComponent(location.fields.hours)}
-        </div>
-      )}
-      {location.fields.phoneNumber && (
-        <div className="flex flex-row my-4">
-          <Image
-            src="/phone.svg"
-            width={26}
-            height={26}
-            alt="Phone number"
-            className="mr-5"
-          />
-          <Text size="small">{location.fields.phoneNumber}</Text>
-        </div>
-      )}
-      {location.fields.streetAddress && location.fields.cityStateZipcode && (
-        <div className="flex flex-row mb-5">
-          <Image
-            src="/map-pin.svg"
-            width={26}
-            height={26}
-            alt="Address"
-            className="mr-5"
-          />
-          <span>
-            <Text size="small">{location.fields.streetAddress}</Text>
-            <Text size="small">{location.fields.cityStateZipcode}</Text>
-          </span>
-        </div>
-      )}
-      {location.fields.button && (
-        <Button
-          text={location.fields.button.fields.text}
-          href={location.fields.button.fields.buttonUrl}
-          className="mx-auto w-2/3 md:w-10/12 uppercase mb-6 md:mb-0"
-          variant={location.fields.button.fields.type}
-          isExternal={location.fields.button.fields.externalLink}
-        />
-      )}
+      <div className="px-2">
+        {location.fields.hours && (
+          <div>{renderRichTextToReactComponent(location.fields.hours)}</div>
+        )}
+        {location.fields.phoneNumber && (
+          <div className="flex flex-row my-4">
+            <Image
+              src="/phone.svg"
+              width={26}
+              height={26}
+              alt="Phone number"
+              className="mr-5"
+            />
+            <Text size="small">{location.fields.phoneNumber}</Text>
+          </div>
+        )}
+        {location.fields.streetAddress && location.fields.cityStateZipcode && (
+          <div className="flex flex-row mb-5">
+            <Image
+              src="/map-pin.svg"
+              width={26}
+              height={26}
+              alt="Address"
+              className="mr-5"
+            />
+            <span>
+              <Text size="small">{location.fields.streetAddress}</Text>
+              <Text size="small">{location.fields.cityStateZipcode}</Text>
+            </span>
+          </div>
+        )}
+        {location.fields.button && (
+          <div className="flex justify-center">
+            <Button
+              text={location.fields.button.fields.text}
+              href={location.fields.button.fields.buttonUrl}
+              className="w-2/3 lg:w-10/12 uppercase mb-6 md:text-center lg:mb-0"
+              variant={location.fields.button.fields.type}
+              isExternal={location.fields.button.fields.externalLink}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
