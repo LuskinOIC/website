ef8df7fdb89798d00a161b8a3981ae4c9900e76e
Yoline <92892101+YolineB@users.noreply.github.com>
2024-05-07 22:58:00 -0700

    Adjust for further padding and alignment on mobile

--- app/components/LocationsCard.tsx
+++ app/components/LocationsCard.tsx
@@ -25,7 +25,8 @@ export default function LocationsCard({ locations }: LocationsCardType) {
 type LocationContentType = { location: LocationType; lastLocation: boolean };
 
 function LocationContent({ location, lastLocation }: LocationContentType) {
-  let outerClassName = "h-full flex flex-col w-full lg:px-6 mb-6 items-center";
+  let outerClassName =
+    "h-full flex flex-col w-full lg:px-6 mb-6 py-6 md:py-0 items-center";
   if (!lastLocation) {
     outerClassName = `${outerClassName} border-b-[1px] md:border-b-0 md:border-r-[3px] border-luskin-green`;
   }
@@ -34,7 +35,7 @@ function LocationContent({ location, lastLocation }: LocationContentType) {
       <Title2 className="md:h-[60px] md:leading-0 lg:text-lg font-bold font-arial pt-0 text-center">
         {location.fields.name}
       </Title2>
-      <div className="px-2">
+      <div className="flex flex-col items-center md:items-start md:px-2">
         {location.fields.hours && (
           <div>{renderRichTextToReactComponent(location.fields.hours)}</div>
         )}
@@ -66,15 +67,13 @@ function LocationContent({ location, lastLocation }: LocationContentType) {
           </div>
         )}
         {location.fields.button && (
-          <div className="flex justify-center">
-            <Button
-              text={location.fields.button.fields.text}
-              href={location.fields.button.fields.buttonUrl}
-              className="w-2/3 lg:w-10/12 uppercase mb-6 md:text-center lg:mb-0"
-              variant={location.fields.button.fields.type}
-              isExternal={location.fields.button.fields.externalLink}
-            />
-          </div>
+          <Button
+            text={location.fields.button.fields.text}
+            href={location.fields.button.fields.buttonUrl}
+            className="place-self-center w-2/3 lg:w-10/12 uppercase mb-6 md:text-center lg:mb-0"
+            variant={location.fields.button.fields.type}
+            isExternal={location.fields.button.fields.externalLink}
+          />
         )}
       </div>
     </div>
