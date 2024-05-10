8c69296b02e1dda7b540cbdc5fc70ac97d7ccbc5
iliadeleon <88496806+iliadeleon@users.noreply.github.com>
2024-05-10 14:03:57 -0400

    May 9- Design Updates (#343)
    * fix urgent care card tablet breakpoint
    
    * update padding between buttons on urgent care card

--- app/specialties/SpecialtyComponents/UrgentCareCard.tsx
+++ app/specialties/SpecialtyComponents/UrgentCareCard.tsx
@@ -12,7 +12,8 @@ import SpecialtyHoursLayout from "./SpecialtyHoursLayout";
 import translations from "@/public/locales/en.json";
 
 const styles = {
-  sectionLayout: "grid lg:grid-cols-2 gap-2 lg:gap-12 p-6 lg:p-12",
+  sectionLayout:
+    "grid min-[1020px]:grid-cols-2 gap-2 min-[1020px]:gap-12 p-6 min-[1020px]:p-12",
   boxStyling: "border border-zinc-300 rounded shadow-md",
 };
 
@@ -33,24 +34,24 @@ export default function UrgentCareCard({ specialty }: SpecialtyCardProps) {
 
   const cardContent = () => {
     return (
-      <div className="grid gap-4 lg:gap-5 justify-items-start lg:py-0">
+      <div className="grid gap-4 min-[1020px]:gap-5 justify-items-start min-[1020px]:py-0">
         <TitleComponent
           title={name}
           titleSize={"Medium"}
           bold={true}
           className=""
         />
-        <div className="grid gap-4 text-base lg:text-lg">
+        <div className="grid gap-4 text-base min-[1020px]:text-lg">
           {descriptionContent}
         </div>
         <Button
-          className="my-3 w-full mx-auto lg:w-auto lg:mx-0"
+          className="my-5 min-[1020px]:my-3 w-full mx-auto min-[1020px]:w-auto min-[1020px]:mx-0"
           href={SAVE_MY_SPOT}
           text="SAVE MY SPOT"
           variant="purple"
           isExternal={true}
         />
-        <div className="grid lg:grid-cols-2 gap-4">
+        <div className="grid min-[1020px]:grid-cols-2 gap-6">
           <Button
             className="text-black bold"
             href={"/patient-care/specialties/urgent-care"}
@@ -66,7 +67,7 @@ export default function UrgentCareCard({ specialty }: SpecialtyCardProps) {
             isExternal={false}
           />
         </div>
-        <div className="block lg:hidden">{walkIns}</div>
+        <div className="block min-[1020px]:hidden">{walkIns}</div>
         <SpecialtyHoursLayout locationContent={location} />
       </div>
     );
@@ -76,9 +77,9 @@ export default function UrgentCareCard({ specialty }: SpecialtyCardProps) {
     <section
       className={`${styles.sectionLayout} ${styles.boxStyling} bg-white`}
     >
-      <div className="order-last lg:order-first">{cardContent()}</div>
+      <div className="order-last min-[1020px]:order-first">{cardContent()}</div>
       {/* desktop image section */}
-      <div className="hidden lg:grid grid-rows-5 gap-5">
+      <div className="hidden min-[1020px]:grid grid-rows-5 gap-5">
         {imageFile.url && (
           <div className="relative w-full row-span-3 mb-6">
             <Image
@@ -90,34 +91,34 @@ export default function UrgentCareCard({ specialty }: SpecialtyCardProps) {
             />
           </div>
         )}
-        <div className="lg:flex flex-col gap-5 row-span-2">
+        <div className="min-[1020px]:flex flex-col gap-5 row-span-2">
           {walkIns}
           <a
             href={englishFormUrl}
             target="_blank"
-            className="underline text-base lg:text-lg font-arial leading-150 "
+            className="underline text-base min-[1020px]:text-lg font-arial leading-150 "
           >
             {translations.urgentCareCard.englishRegistrationFormText}
           </a>
           <a
             href={spanishFormUrl}
             target="_blank"
-            className="underline text-base lg:text-lg font-arial leading-150 "
+            className="underline text-base min-[1020px]:text-lg font-arial leading-150 "
           >
             {translations.urgentCareCard.spanishRegistrationFormText}
           </a>
         </div>
       </div>
       {/* mobile image section */}
-      <div className="block lg:hidden">
+      <div className="block min-[1020px]:hidden">
         {imageFile.url && (
-          <div className="w-full lg:hidden">
+          <div className="w-full min-[1020px]:hidden">
             <Image
               src={`https:${imageFile.url}`}
               alt={imageTitle}
               width={imageFile.details.image.width}
               height={imageFile.details.image.height}
-              style={{ width: "100%", height: "175px", objectFit: "fill" }}
+              style={{ width: "100%", maxHeight: "260px", objectFit: "fill" }}
             />
           </div>
         )}
