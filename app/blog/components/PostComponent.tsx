47b471718c411a1fe5046aab83f02048fd0ab102
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-05-14 19:23:59 -0700

    removed excess padding from title, byline, subtitle

--- app/blog/components/PostComponent.tsx
+++ app/blog/components/PostComponent.tsx
@@ -14,16 +14,13 @@ type PostComponentProps = {
 const styles = {
   sectionWrapper: "px-5 md:px-32 md:mx-auto md:w-10/12 py-1.5 md:py-4",
   postDetailsContainer: "flex flex-col md:flex-row md:gap-12 px-5 md:px-32",
-  postDetailsWrapper: "flex flex-row md:flex-col gap-2 py-1.5 md:py-4 ",
-  // 20px font size, regular, dark grey #868787
+  postDetailsWrapper: "flex flex-row md:flex-col gap-2 py-1.5",
   postPublishedFont: "font-bold",
   postDetailsFont: "font-regular md:text-[20px] text-[#868787]",
-  // socialMediaWrapper: "flex flex-col md:flex-row gap-2 py-1.5 md:py-4",
-  // change font size to 36px bold
   postTitle:
-    "text-2xl md:text-[36px] py-2 md:py-6 font-bold md:leading-relaxed",
+    "text-2xl md:text-[36px] py-2 md:pt-6 font-bold md:leading-relaxed",
   postSubTitle:
-    "text-xl md:text-2xl font-arial leading-[36px] md:leading-[40px] py-2",
+    "text-xl md:text-2xl font-arial font-bold leading-[36px] md:leading-[40px] pt-6 pb-2",
   postImage: "w-full -order-1 md:order-none",
   postPadding: "",
   dividerStyle: "block md:hidden h-[4px] bg-[#99C221] mx-4 mt-3",
@@ -49,7 +46,8 @@ const PostComponent = ({ postData }: PostComponentProps) => {
 
   return (
     <>
-      <PageSectionContainer showBottomMargin={true} showTopMargin={true}>
+      {/* <PageSectionContainer showBottomMargin={true} showTopMargin={true}> */}
+      <PageSectionContainer showTopMargin={true}>
         <Title1 className={styles.postTitle}>{title}</Title1>
       </PageSectionContainer>
       <PageSectionContainer>
@@ -64,7 +62,7 @@ const PostComponent = ({ postData }: PostComponentProps) => {
           </div>
         </div>
       </PageSectionContainer>
-      <PageSectionContainer showBottomMargin={true}>
+      <PageSectionContainer>
         <Image
           className={styles.postImage}
           src={url}
@@ -85,7 +83,8 @@ const PostComponent = ({ postData }: PostComponentProps) => {
             section.fields.columnLayout.fields.columnType === "Full Column"
               ? `${styles.postPadding}`
               : ""
-          }`}>
+          }`}
+        >
           <PageSection key={section.fields.title} section={section} />
         </div>
       ))}
