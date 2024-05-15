c4d2f816449d08d838c54269daaa44e284274352
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-05-14 19:05:09 -0700

    moved byline inbetween title and image, moved subtitle under image

--- app/blog/components/PostComponent.tsx
+++ app/blog/components/PostComponent.tsx
@@ -15,10 +15,13 @@ const styles = {
   sectionWrapper: "px-5 md:px-32 md:mx-auto md:w-10/12 py-1.5 md:py-4",
   postDetailsContainer: "flex flex-col md:flex-row md:gap-12 px-5 md:px-32",
   postDetailsWrapper: "flex flex-row md:flex-col gap-2 py-1.5 md:py-4 ",
-  postDetailsFont: "font-bold",
-  socialMediaWrapper: "flex flex-col md:flex-row gap-2 py-1.5 md:py-4",
+  // 20px font size, regular, dark grey #868787
+  postPublishedFont: "font-bold",
+  postDetailsFont: "font-regular md:text-[20px] text-[#868787]",
+  // socialMediaWrapper: "flex flex-col md:flex-row gap-2 py-1.5 md:py-4",
+  // change font size to 36px bold
   postTitle:
-    "text-2xl md:text-[40px] py-2 md:py-6 font-normal md:leading-relaxed",
+    "text-2xl md:text-[36px] py-2 md:py-6 font-bold md:leading-relaxed",
   postSubTitle:
     "text-xl md:text-2xl font-arial leading-[36px] md:leading-[40px] py-2",
   postImage: "w-full -order-1 md:order-none",
@@ -48,7 +51,18 @@ const PostComponent = ({ postData }: PostComponentProps) => {
     <>
       <PageSectionContainer showBottomMargin={true} showTopMargin={true}>
         <Title1 className={styles.postTitle}>{title}</Title1>
-        <div className={styles.postSubTitle}>{newsSubTitle}</div>
+      </PageSectionContainer>
+      <PageSectionContainer>
+        <div className={`${styles.postDetailsWrapper}`}>
+          <div className={`${styles.postDetailsFont}`}>
+            <span className={`${styles.postPublishedFont}`}>
+              {translations.blog.publishedOn}
+            </span>
+            {` ${writtenBy}`}
+            <span> &#x2022;</span>
+            {` ${formattedDate}`}
+          </div>
+        </div>
       </PageSectionContainer>
       <PageSectionContainer showBottomMargin={true}>
         <Image
@@ -58,32 +72,7 @@ const PostComponent = ({ postData }: PostComponentProps) => {
           width={details.image.width}
           height={details.image.height}
         />
-      </PageSectionContainer>
-      <PageSectionContainer>
-        <div className="grid md:grid-cols-3">
-          <div className={`${styles.postDetailsWrapper}`}>
-            <div className={`${styles.postDetailsFont}`}>
-              {translations.blog.publishedOn}
-            </div>
-            <div>{formattedDate}</div>
-          </div>
-          <div className={`${styles.postDetailsWrapper}`}>
-            <div className={`${styles.postDetailsFont}`}>
-              {translations.blog.writtenBy}
-            </div>
-            <div>{writtenBy}</div>
-          </div>
-          {followOurStory && (
-            <div className={`${styles.socialMediaWrapper}`}>
-              <div className="font-bold">
-                {translations.blog.followOurStory}
-              </div>
-              {followOurStory && (
-                <SocialMediaSection section={followOurStory} />
-              )}
-            </div>
-          )}
-        </div>
+        <div className={styles.postSubTitle}>{newsSubTitle}</div>
       </PageSectionContainer>
 
       <hr className={styles.dividerStyle} />
@@ -96,8 +85,7 @@ const PostComponent = ({ postData }: PostComponentProps) => {
             section.fields.columnLayout.fields.columnType === "Full Column"
               ? `${styles.postPadding}`
               : ""
-          }`}
-        >
+          }`}>
           <PageSection key={section.fields.title} section={section} />
         </div>
       ))}
