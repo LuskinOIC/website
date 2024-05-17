567a9aa86362a635ff69e8caa1b20025a8c55cd6
Shane Jeon <88866019+shane-jeon@users.noreply.github.com>
2024-05-17 15:33:34 -0700

    Blog Info Details (#347)
    * changed 'publishedOn': 'Published On' to 'publishedBy': 'Published By' in en.json file. Altered styling to reflect Figma design. 
    
    
    ---------
    
    Co-authored-by: Shane Jeon <88866019+codingtokki@users.noreply.github.com>

--- app/blog/components/PostComponent.tsx
+++ app/blog/components/PostComponent.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import renderRichTextToReactComponent from "@/app/utils/rich-text";
 import { BlogPostType, PageSectionType } from "@/app/constants/types";
 import { Title1 } from "@/app/components/ui/Typography/Title";
-import SocialMediaSection from "@/app/components/SocialMediaSection";
 import PageSection from "@/app/components/PageSection/PageSection";
 import { PageSectionContainer } from "@/app/components/PageSection/PageSection";
 import translations from "@/public/locales/en.json";
@@ -14,28 +13,21 @@ type PostComponentProps = {
 const styles = {
   sectionWrapper: "px-5 md:px-32 md:mx-auto md:w-10/12 py-1.5 md:py-4",
   postDetailsContainer: "flex flex-col md:flex-row md:gap-12 px-5 md:px-32",
-  postDetailsWrapper: "flex flex-row md:flex-col gap-2 py-1.5 md:py-4 ",
-  postDetailsFont: "font-bold",
-  socialMediaWrapper: "flex flex-col md:flex-row gap-2 py-1.5 md:py-4",
+  postDetailsWrapper: "flex flex-row md:flex-col gap-2 py-1.5",
+  postPublishedFont: "font-bold",
+  postDetailsFont: "font-regular md:text-[20px] text-[#868787]",
   postTitle:
-    "text-2xl md:text-[40px] py-2 md:py-6 font-normal md:leading-relaxed",
+    "text-2xl md:text-[36px] py-2 md:pt-6 font-bold md:leading-relaxed",
   postSubTitle:
-    "text-xl md:text-2xl font-arial leading-[36px] md:leading-[40px] py-2",
+    "text-xl md:text-2xl font-arial font-bold leading-[36px] md:leading-[40px] pt-6 pb-2",
   postImage: "w-full -order-1 md:order-none",
   postPadding: "",
   dividerStyle: "block md:hidden h-[4px] bg-[#99C221] mx-4 mt-3",
 };
 
 const PostComponent = ({ postData }: PostComponentProps) => {
-  const {
-    title,
-    subTitle,
-    date,
-    writtenBy,
-    followOurStory,
-    mainImage,
-    pageSections,
-  } = postData.fields;
+  const { title, subTitle, date, writtenBy, mainImage, pageSections } =
+    postData.fields;
 
   const { url, details, fileName } = mainImage.fields.file;
   const newsSubTitle = renderRichTextToReactComponent(subTitle);
@@ -46,11 +38,22 @@ const PostComponent = ({ postData }: PostComponentProps) => {
 
   return (
     <>
-      <PageSectionContainer showBottomMargin={true} showTopMargin={true}>
+      <PageSectionContainer showTopMargin={true}>
         <Title1 className={styles.postTitle}>{title}</Title1>
-        <div className={styles.postSubTitle}>{newsSubTitle}</div>
       </PageSectionContainer>
-      <PageSectionContainer showBottomMargin={true}>
+      <PageSectionContainer>
+        <div className={`${styles.postDetailsWrapper}`}>
+          <div className={`${styles.postDetailsFont}`}>
+            <span className={`${styles.postPublishedFont}`}>
+              {translations.blog.publishedBy}
+            </span>
+            {` ${writtenBy}`}
+            <span> &#x2022;</span>
+            {` ${formattedDate}`}
+          </div>
+        </div>
+      </PageSectionContainer>
+      <PageSectionContainer>
         <Image
           className={styles.postImage}
           src={url}
@@ -58,32 +61,7 @@ const PostComponent = ({ postData }: PostComponentProps) => {
           width={details.image.width}
           height={details.image.height}
         />
-      </PageSectionContainer>
-      <PageSectionContainer>
-        <div className="grid md:grid-cols-3">
-          <div className={`${styles.postDetailsWrapper}`}>
-            <div className={`${styles.postDetailsFont}`}>
-              {translations.blog.publishedOn}
-            </div>
-            <div>{formattedDate}</div>
-          </div>
-          <div className={`${styles.postDetailsWrapper}`}>
-            <div className={`${styles.postDetailsFont}`}>
-              {translations.blog.writtenBy}
-            </div>
-            <div>{writtenBy}</div>
-          </div>
-          {followOurStory && (
-            <div className={`${styles.socialMediaWrapper}`}>
-              <div className="font-bold">
-                {translations.blog.followOurStory}
-              </div>
-              {followOurStory && (
-                <SocialMediaSection section={followOurStory} />
-              )}
-            </div>
-          )}
-        </div>
+        <div className={styles.postSubTitle}>{newsSubTitle}</div>
       </PageSectionContainer>
 
       <hr className={styles.dividerStyle} />
