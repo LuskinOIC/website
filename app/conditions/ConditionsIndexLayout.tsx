f4de252a342986811a600358346590095398cd59
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2024-05-21 21:20:56 -0700

    cleaned up code

--- app/conditions/ConditionsIndexLayout.tsx
+++ app/conditions/ConditionsIndexLayout.tsx
@@ -14,7 +14,6 @@ const styles = {
     "hidden md:block h-fit bg-[#F1F1F1] text-[24px] p-4 rounded mr-12",
   letter: "flex flex-cols justify-center p-1 bold hover:text-[#0076AD] hover:underline",
   termsContainer: "flex-grow grid grid-cols-1 gap-5 overflow-auto max-h-[calc(73rem)] p-4",
-  disabledLetter: "text-gray-400 cursor-not-allowed"
 };
 
 export default async function ConditionsIndexLayout() {
@@ -34,7 +33,6 @@ export default async function ConditionsIndexLayout() {
   }
 
   const groupedConditions = groupTermsByFirstLetter(conditions);
-  console.log('this is the GroupedConditions ===>', groupedConditions)
 
   const GlossarySection = () => {
     return (
@@ -58,11 +56,13 @@ export default async function ConditionsIndexLayout() {
       {/* TO DO: Search + Glossary Index BTN */}
       {/* <SearchBar /> */}
       <div className={styles.lettersContainer}>
-        {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => (
-          <a href={`#${letter}`} key={letter} className={styles.letter}>
-            {letter}
-          </a>
-        ))}
+        {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => {
+          return (
+            <a href={`#${letter}`} key={letter} className={styles.letter}>
+              {letter}
+            </a>
+          );
+        })}
       </div>
       <div className={styles.termsContainer}>{GlossarySection()}</div>
     </section>
