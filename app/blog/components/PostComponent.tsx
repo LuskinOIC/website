591c0794436d9a128dba0c10b074d5a19a780665
Yoline <92892101+YolineB@users.noreply.github.com>
2024-05-23 11:59:44 -0700

    Remove subtitle, adjust details padding (#351)

--- app/blog/components/PostComponent.tsx
+++ app/blog/components/PostComponent.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import renderRichTextToReactComponent from "@/app/utils/rich-text";
 import { BlogPostType, PageSectionType } from "@/app/constants/types";
 import { Title1 } from "@/app/components/ui/Typography/Title";
 import PageSection from "@/app/components/PageSection/PageSection";
@@ -12,25 +11,20 @@ type PostComponentProps = {
 
 const styles = {
   sectionWrapper: "px-5 md:px-32 md:mx-auto md:w-10/12 py-1.5 md:py-4",
-  postDetailsContainer: "flex flex-col md:flex-row md:gap-12 px-5 md:px-32",
-  postDetailsWrapper: "flex flex-row md:flex-col gap-2 py-1.5",
+  postDetailsContainer: "flex flex-col md:flex-row px-5 md:px-32",
+  postDetailsWrapper: "flex flex-row md:flex-col py-1.5 md:py-2.5",
   postPublishedFont: "font-bold",
   postDetailsFont: "font-regular md:text-[20px] text-[#868787]",
-  postTitle:
-    "text-2xl md:text-[36px] py-2 md:pt-6 font-bold md:leading-relaxed",
-  postSubTitle:
-    "text-xl md:text-2xl font-arial font-bold leading-[36px] md:leading-[40px] pt-6 pb-2",
+  postTitle: "py-0 text-2xl md:text-[36px] md:pt-6 md:pb-0 font-bold",
   postImage: "w-full -order-1 md:order-none",
   postPadding: "",
-  dividerStyle: "block md:hidden h-[4px] bg-[#99C221] mx-4 mt-3",
 };
 
 const PostComponent = ({ postData }: PostComponentProps) => {
-  const { title, subTitle, date, writtenBy, mainImage, pageSections } =
-    postData.fields;
+  const { title, date, writtenBy, mainImage, pageSections } =
+    postData?.fields ?? {};
 
   const { url, details, fileName } = mainImage.fields.file;
-  const newsSubTitle = renderRichTextToReactComponent(subTitle);
   const dateDate = new Date(date);
   const formattedDate = `${
     dateDate.getMonth() + 1
@@ -53,7 +47,7 @@ const PostComponent = ({ postData }: PostComponentProps) => {
           </div>
         </div>
       </PageSectionContainer>
-      <PageSectionContainer>
+      <PageSectionContainer showTopPadding={true} showBottomPadding={true}>
         <Image
           className={styles.postImage}
           src={url}
@@ -61,10 +55,7 @@ const PostComponent = ({ postData }: PostComponentProps) => {
           width={details.image.width}
           height={details.image.height}
         />
-        <div className={styles.postSubTitle}>{newsSubTitle}</div>
       </PageSectionContainer>
-
-      <hr className={styles.dividerStyle} />
 
       {pageSections.map((section: PageSectionType, i: string) => (
         <div
