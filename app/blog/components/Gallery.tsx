86ad76aaa0f35924ba3dfa55a0ac38e3e1a8e1a7
Yoline <92892101+YolineB@users.noreply.github.com>
2024-06-07 10:51:46 -0700

    Blog index redesign - Gallery (#355)
    * Correct metadata description typo
    
    * Remove pageSection references
    
    * Add Blog Selector
    
    * Add gallery, news and insights posts only
    
    * Update for events and patient stories
    
    * Reroute /news/events /patient-stories to /blog

---
+++ app/blog/components/Gallery.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { BlogPostType } from "@/app/constants/types";
+
+interface Props {
+  type: string;
+  posts: BlogPostType[];
+}
+
+const styles = {
+  gridContainer: "grid lg:grid-cols-3 gap-4 pt-6 lg:pt-10",
+  postsItem: "relative w-full h-[300px] rounded-xl",
+  fullSpan: "lg:col-span-3",
+  oneColumn: "lg:col-span-1",
+  twoColumn: "lg:col-span-2",
+  imageOverlay: "absolute bottom-0 left-0 text-white text-2xl p-2 rounded",
+  imageContainer: "w-full h-full overflow-hidden rounded-xl",
+  image: "w-full h-full object-cover rounded-xl",
+  overlayGradient:
+    "absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent z-0 rounded-xl hover:shadow-2xl pt-56",
+};
+
+const getClassNames = (index: number, isLast: boolean) => {
+  if (isLast && index % 2 != 0) {
+    return styles.fullSpan;
+  }
+
+  switch (index % 7) {
+    case 0:
+      return styles.fullSpan;
+    case 1:
+      return styles.oneColumn;
+    case 2:
+      return styles.twoColumn;
+    case 3:
+      return styles.twoColumn;
+    case 4:
+      return styles.oneColumn;
+    case 5:
+      return styles.oneColumn;
+    case 6:
+      return styles.twoColumn;
+    default:
+      return "";
+  }
+};
+
+const Gallery = ({ type, posts }: Props) => {
+  return (
+    <div className={styles.gridContainer}>
+      {posts &&
+        posts.map((post: any, index: number) => (
+          <div
+            key={post.slug}
+            className={`${styles.postsItem} ${getClassNames(
+              index,
+              index === posts.length - 1,
+            )}`}
+          >
+            <Link
+              href={`/${type}/${post.slug}`}
+              className={styles.imageContainer}
+            >
+              <Image
+                src={post.mainImage.fields.file.url}
+                alt={post.title}
+                width={500}
+                height={300}
+                className={styles.image}
+              />
+              <div className={styles.overlayGradient}></div>
+              <div className={styles.imageOverlay}>{post.title}</div>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Gallery;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,75 +1,37 @@
-import PageSection from "@/app/components/PageSection/PageSection";
 import {
-  PageSectionType,
-  BlogCardsRowType,
-  OptionType,
-} from "@/app/constants/types";
-import {
-  getPageByType,
   getNewsPosts,
   getEvents,
   getPatientStories,
   getInsightsPosts,
 } from "@/app/utils/contentful";
-import { PAGE_TYPES } from "@/app/constants/entries";
-import BlogCardsRow from "../components/BlogCardsRow";
-import Dropdown from "../components/ui/Dropdown";
-import translations from "@/public/locales/en.json";
 import { PageSectionContainer } from "@/app/components/PageSection/PageSection";
+import BlogIndex from "@/app/blog/components/BlogIndex";
 
 export function generateMetadata() {
   return {
     title: "LuskinOIC Blog",
     description:
-      "Read the Luskin OICblog to browse patient stories, explore insights, news & events as they relate to the Luskin Orthopaedic Institute for Children.",
+      "Read the LuskinOIC blog to browse patient stories, explore insights, news & events as they relate to the Luskin Orthopaedic Institute for Children.",
   };
 }
 
 export default async function Blog() {
-  const page = await getPageByType(PAGE_TYPES.BLOG);
-  const news = (await getNewsPosts(8)) as unknown as BlogCardsRowType[];
-  const insights = (await getInsightsPosts(8)) as unknown as BlogCardsRowType[];
-  const events = (await getEvents(8)) as unknown as BlogCardsRowType[];
-  const patientStories = (await getPatientStories(
-    8,
-  )) as unknown as BlogCardsRowType[];
+  // TO DO: REPLACE TYPE
+  const news = (await getNewsPosts()) as any;
+  const patientStories = (await getPatientStories()) as any;
+  const insights = (await getInsightsPosts()) as any;
+  const events = (await getEvents()) as any;
 
-  const dropdownOptions: OptionType[] = [
-    { label: "News", value: "news", targetID: "news" },
-    { label: "Insights", value: "insights", targetID: "insights" },
-    { label: "Events", value: "events", targetID: "events" },
-    {
-      label: "Patient Stories",
-      value: "patient-stories",
-      targetID: "patient-stories",
-    },
-  ];
+  const blogData = {
+    news,
+    patientStories,
+    insights,
+    events,
+  };
 
   return (
-    <div>
-      <div className="mb-12 md:hidden px-5">
-        <p className="px-1 pb-4">{translations.blog.sectionReviewText}</p>
-        <Dropdown placeHolder="News" options={dropdownOptions} />
-      </div>
-
-      {/* TODO: Move into associated sections */}
-      <PageSectionContainer>
-        <BlogCardsRow type="news" cards={news} />
-      </PageSectionContainer>
-      <PageSectionContainer>
-        <BlogCardsRow type="insights" cards={insights} />
-      </PageSectionContainer>
-      <PageSectionContainer>
-        <BlogCardsRow type="events" cards={events} />
-      </PageSectionContainer>
-
-      {page.pageSections.map((pageSection: PageSectionType) => (
-        <PageSection key={pageSection.fields.title} section={pageSection} />
-      ))}
-
-      <PageSectionContainer>
-        <BlogCardsRow type="patient-stories" cards={patientStories} />
-      </PageSectionContainer>
-    </div>
+    <PageSectionContainer showTopMargin={true}>
+      <BlogIndex blogData={blogData} />
+    </PageSectionContainer>
   );
 }
