dd9e2cacf24f97a3ffa980520555902cf51f84d7
Yoline <92892101+YolineB@users.noreply.github.com>
2024-07-05 11:18:00 -0700

    Update gallery test to show news and insights

--- app/gallery-test/page.tsx
+++ app/gallery-test/page.tsx
@@ -1,7 +1,12 @@
 // TO BE DELTED AFTER ILIA CONFIRMATION
 
 import { BlogPostType } from "@/app/constants/types";
-import { getPatientStories, getEvents } from "@/app/utils/contentful";
+import {
+  getPatientStories,
+  getEvents,
+  getNewsPosts,
+  getInsightsPosts,
+} from "@/app/utils/contentful";
 import { PageSectionContainer } from "@/app/components/PageSection/PageSection";
 import BlogIndex from "./BlogIndex";
 
@@ -9,8 +14,12 @@ export default async function PatientStories() {
   const patientStories =
     (await getPatientStories()) as unknown as BlogPostType[];
   const events = (await getEvents()) as unknown as BlogPostType[];
+  const news = (await getNewsPosts()) as unknown as BlogPostType[];
+  const insights = (await getInsightsPosts()) as unknown as BlogPostType[];
 
   const blogData = {
+    insights,
+    news,
     patientStories,
     events,
   };
