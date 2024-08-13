7c091436f4168e7bebd10b8659ab7eb54f4520ca
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-08-13 10:49:14 -0700

    git reset soft

--- app/components/BlogCardsRow.tsx
+++ app/components/BlogCardsRow.tsx
@@ -4,8 +4,6 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { BlogCardsRowType } from "@/app/constants/types";
 import BlogCard from "@/app/components/PageSection/CardsLayout/BlogCard";
-import { Title1, Title3 } from "@/app/components/ui/Typography/Title";
-import translations from "@/public/locales/en.json";
 
 type BlogCardsRowPropsType = {
   type: string;
@@ -17,8 +15,7 @@ const isMobileScreen = () => window.innerWidth <= 768;
 
 export default function BlogCardsRow({
   type,
-  cards,
-  showIndexLinks = true,
+  cards, // showIndexLinks = true,
 }: BlogCardsRowPropsType) {
   const [displayedCards, setDisplayedCards] = useState(cards);
 
@@ -41,25 +38,11 @@ export default function BlogCardsRow({
     return () => window.removeEventListener("resize", handleResize);
   }, [cards]);
 
+  console.log(displayedCards[0]);
+
   return (
     <div id={`${type}`} className="block">
-      {showIndexLinks && (
-        <div className="border- mb-2 mt-8 flex flex-row items-center justify-between md:mb-4">
-          <Link href={`/${type}`}>
-            <Title1 className="text-2xl font-bold uppercase text-[#0076AD] hover:underline md:font-normal md:capitalize">
-              {type === "patient-stories" ? "patient stories" : type}
-            </Title1>
-          </Link>
-          <Link href={`/${type}`}>
-            <Title3 className="block uppercase text-[#0076AD] underline md:no-underline md:hover:underline">
-              {translations.blog.viewAll}
-            </Title3>
-          </Link>
-        </div>
-      )}
-      <div
-        className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8`}
-      >
+      <div className="grid grid-cols-2 gap-4">
         {displayedCards.map((card: BlogCardsRowType) => (
           <Link key={card.slug} href={`/${type}/${card.slug}`}>
             <BlogCard
@@ -68,6 +51,8 @@ export default function BlogCardsRow({
                 title: card.blogCard.fields.title,
                 cardPhoto: card.blogCard.fields.image,
                 summary: card.blogCard.fields.subTitle || "",
+                writtenBy: card.writtenBy || "",
+                date: card.date,
               }}
             />
           </Link>
